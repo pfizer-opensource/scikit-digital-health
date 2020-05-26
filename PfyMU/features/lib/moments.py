16298d5ede97e3a8e980e378d08f9fa03cf55dd1
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-05-26 10:37:44 -0400

    Add mean cross rate

--- PfyMU/features/lib/moments.py
+++ PfyMU/features/lib/moments.py
@@ -4,7 +4,7 @@ Features from statistical moments
 Lukas Adamowicz
 Pfizer DMTI 2020
 """
-from numpy import mean, std
+from numpy import mean, std, sum, diff, sign
 from scipy.stats import skew, kurtosis
 
 from PfyMU.features.core import Feature
@@ -32,6 +32,27 @@ class Mean(Feature):
         super()._compute(x, fs)
 
         self._result = mean(x, axis=1)
+
+
+class MeanCrossRate(Feature):
+    def __init__(self):
+        """
+        Calculate the number of times the signal crosses the mean value, expressed as a percentage of the signal
+        length in samples.
+
+        Methods
+        -------
+        compute(signal[, columns=None])
+        """
+        super(MeanCrossRate, self).__init__('MeanCrossRate', {})
+
+    def _compute(self, x, fs):
+        super(MeanCrossRate, self)._compute(x, fs)
+
+        x_nomean = x - mean(x, axis=1, keepdims=True)
+        mcr = sum(diff(sign(x_nomean), axis=1) != 0, axis=1)
+
+        self._result = mcr / x.shape[1]  # shape of the 1 axis
 
 
 class StdDev(Feature):
