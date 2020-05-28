a20e96067a8f69906bbdb61e565d7f3debada6a9
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-05-28 14:31:58 -0400

    remove cy from name

--- PfyMU/features/lib/_cython/__init__.py
+++ PfyMU/features/lib/_cython/__init__.py
@@ -1,1 +1,7 @@
+from cy_complexityInvariantDistance import cy_CID
+from cy_correlationCoefficient import cy_CC3
+from cy_entropy import cy_SignalEntropy, cy_SampleEntropy
+from cy_frequencyFeatures import cy_FrequencyFeatures
+from cy_jerk import cy_JerkMetric, cy_DimensionlessJerk
 from cy_linearSlope import cy_LinRegression
+from cy_rangeCount import cy_RangeCount
