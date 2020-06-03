319c35fbe0e7a12ec56e1ddd002e7c55dc4424a5
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-06-03 08:38:29 -0400

    Add MCR, stdev, skewness, kurtosis tests

--- PfyMU/tests/features/test_features.py
+++ PfyMU/tests/features/test_features.py
@@ -5,3 +5,19 @@ from PfyMU.tests.features.conftest import TestFeature
 
 class TestMean(TestFeature):
     feature = Mean()
+
+
+class TestMeanCrossRate(TestFeature):
+    feature = MeanCrossRate()
+
+
+class TestStdDev(TestFeature):
+    feature = StdDev()
+
+
+class TestSkewness(TestFeature):
+    feature = Skewness()
+
+
+class TestKurtosis(TestFeature):
+    feature = Kurtosis()
