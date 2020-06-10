e7526e7c73f9652e5d475d3a7c471e686d71651b
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-06-10 09:43:19 -0400

    Add testing for dataset loading

--- PfyMU/tests/gait/test_train_classifier.py
+++ PfyMU/tests/gait/test_train_classifier.py
@@ -1,2 +1,51 @@
+from pytest import mark
+
+from PfyMU.features.utility import compute_window_samples
 from PfyMU.gait.train_classifier.core import *
+
+
+class TestLoadDatasets:
+    @mark.parametrize(
+        ('gfs', 'wlen_s', 'wstep'),
+        (
+                (30.0, 3.0, 0.5),
+                (100.0, 4.0, 0.1)
+        )
+    )
+    def test_2_datasets(self, sample_datasets, gfs, wlen_s, wstep):
+        wlen_n, wstep_n = compute_window_samples(gfs, wlen_s, wstep)
+
+        dataset, subjects, labels = load_datasets(
+            sample_datasets,
+            goal_fs=gfs,
+            acc_mag=True,
+            window_length=wlen_s,
+            window_step=wstep
+        )
+
+        # compute number of expected windows, etc
+        s_n1 = 2  # subjects in study 1
+        act_n1 = 3  # activities
+        tr_n1 = 2  # trials per activity
+        fs1 = 100.0  # sampling rate
+        ns1 = 1500  # number of samples
+        s_n2 = 2
+        act_n2 = 2
+        tr_n2 = 3
+        fs2 = 50.0
+        ns2 = 1000
+
+        N1 = int(round(ns1 * gfs / fs1))
+        N2 = int(round(ns2 * gfs / fs2))
+
+        nw1 = ((N1 - wlen_n) // wstep_n + 1) * s_n1 * act_n1 * tr_n1
+        nw2 = ((N2 - wlen_n) // wstep_n + 1) * s_n2 * act_n2 * tr_n2
+
+        n_win = nw1 + nw2
+
+        assert dataset.shape == (n_win, wlen_n)
+        assert subjects[0] == 'subject_0_0'
+        assert subjects[-1] == 'subject_1_1'
+
+
 
