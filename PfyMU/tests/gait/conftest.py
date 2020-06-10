19688167b4090026580720f9cb6b1c5c866e3124
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-06-10 09:43:10 -0400

    Fix some typos

--- PfyMU/tests/gait/conftest.py
+++ PfyMU/tests/gait/conftest.py
@@ -5,15 +5,15 @@ import h5py
 import numpy as np
 
 
-@fixture()
+@fixture(scope='class')
 def sample_datasets():
     study1_td = TempDir()
-    study1_path = Path(study1_td)
+    study1_path = Path(study1_td.name)
     study2_td = TempDir()
-    study2_path = Path(study2_td)
+    study2_path = Path(study2_td.name)
 
-    # study 1
     for k in range(2):
+        # study 1
         with h5py.File(study1_path / f'subject_{k}.h5', 'w') as f:
             for j in range(3):
                 ag = f.create_group(f'activity{j}')
@@ -26,17 +26,16 @@ def sample_datasets():
                     agt.create_dataset('Accelerometer', data=np.random.rand(1500, 3) + np.array([[0, 0, 1]]))
 
     # study 2
-    for k in range(2):
         with h5py.File(study2_path / f'subject_{k}.h5', 'w') as f:
-            for j in range(3):
+            for j in range(2):
                 ag = f.create_group(f'activity{j}')
                 ag.attrs.create('Gait Label', 1 if j == 1 else 0)
 
-                for i in range(2):
+                for i in range(3):
                     agt = ag.create_group(f'Trial {i}')
                     agt.attrs.create('Sampling rate', 50.0)
 
-                    agt.create_dataset('Accelerometer', data=np.random.rand(1000, 3) + np.array([[0, 0, 1]]))
+                    agt.create_dataset('Accelerometer', data=np.random.rand(1000, 3) + np.array([[0, 1, 0]]))
 
     yield [study1_path, study2_path]
 
