c1842a942c534f4b0e1e7eb2fb536ec2a58f6a3d
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-06-26 10:30:30 -0400

    Add different overlap for gait/non-gait possibility Add return of specific activity type

--- PfyMU/gait/train_classifier/core.py
+++ PfyMU/gait/train_classifier/core.py
@@ -27,9 +27,10 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
         Compute and use the acceleration magnitude instead of the 3-axis acceleration. Default is True.
     window_length : float
         Window length in seconds. If not provided (None), will do no windowing. Default is 3.0 seconds
-    window_step : {float, int}
+    window_step : {float, int, array-like}
         Window step - the spacing between the start of windows. This can be specified several different ways
-        (see Notes). Default is 0.5
+        (see Notes). Default is 0.5. If providing an array, the first value is the overlap for NON-GAIT events, and the
+        second value is the overlap for GAIT events.
 
     Returns
     -------
@@ -37,12 +38,14 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
         (M, N, P) array of acceleration data windowed as specified. M is the total number of windows accross all
         datasets loaded. N is the window length, and P is the number of axes (3 for non-magnitude, 1 for using
         acceleration magnitude)
+    labels : numpy.ndarray
+        (M, ) array of labels as to whether the window contains gait data or not.
     subjects : numpy.ndarray
         (M, ) array of subject identifiers. In order to ensure that these are distinct between studies, the number of
         the study (determined by order in `paths`) is appended to the subject name, e.g. `subject1_3` would be
-        `subject` for the 3rd study in `paths`.
-    labels : numpy.ndarray
-        (M, ) array of labels as to whether the window contains gait data or not.
+        `subject1` for the 3rd study in `paths`.
+    activities : numpy.ndarray
+        (M, ) array of the specific activity identifiers.
 
     Notes
     -----
@@ -63,7 +66,14 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
     paths = [Path(i) for i in paths]  # make sure entries are Path objects
 
     # compute the goal window length and the goal window step
-    n_wlen, n_wstep = compute_window_samples(goal_fs, window_length, window_step)
+    if hasattr(window_step, '__len__'):
+        n_wstep = [0, 0]
+        n_wlen, n_wstep[0] = compute_window_samples(goal_fs, window_length, window_step[0])
+        n_wlen, n_wstep[1] = compute_window_samples(goal_fs, window_length, window_step[1])
+        step2 = True
+    else:
+        n_wlen, n_wstep = compute_window_samples(goal_fs, window_length, window_step)
+        step2 = False
 
     M, N = 0, n_wlen
 
@@ -75,6 +85,7 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
         for subj in subjs:
             with h5py.File(subj, 'r') as f:
                 for activity in f.keys():
+                    gait_label = f[activity].attrs.get('Gait label')
                     for trial in f[activity].keys():
                         n = f[activity][trial]['Accelerometer'].shape[0]
                         fs = f[activity][trial].attrs.get('Sampling rate')
@@ -82,11 +93,14 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
                         n = int(np.ceil(n * goal_fs / fs))  # compute samples when down/upsampled
                         if n < n_wlen:
                             continue
-                        M += int(((n - n_wlen) // n_wstep + 1))
-
+                        if step2:
+                            M += int(((n - n_wlen) // n_wstep[gait_label] + 1))
+                        else:
+                            M += int(((n - n_wlen) // n_wstep + 1))
     # allocate space for the data
     dataset = np.zeros((M, N)) if acc_mag else np.zeros((M, N, 3))
     subjects = np.empty(M, dtype='U30')  # maximum 30 character strings
+    activities = np.empty(M, dtype='U30')  # maximum 30 character strings
     labels = np.empty(M, dtype='int')
 
     cnt = 0  # keeping track of index
@@ -121,15 +135,22 @@ def load_datasets(paths, goal_fs=100.0, acc_mag=True, window_length=3.0, window_
 
                         if acc_mag:
                             tmp = np.linalg.norm(tmp, axis=1)
-                        m = int(((tmp.shape[0] - n_wlen) // n_wstep + 1))
-                        dataset[cnt:cnt+m] = get_windowed_view(tmp, n_wlen, n_wstep)
+
+                        if step2:
+                            m = int(((tmp.shape[0] - n_wlen) // n_wstep[gait_label] + 1))
+                            dataset[cnt:cnt + m] = get_windowed_view(tmp, n_wlen, n_wstep[gait_label])
+                        else:
+                            m = int(((tmp.shape[0] - n_wlen) // n_wstep + 1))
+                            dataset[cnt:cnt + m] = get_windowed_view(tmp, n_wlen, n_wstep)
+
                         # append study/dataset number to seperate studies
                         subjects[cnt:cnt+m] = f'{subj.name.split(".")[0]}_{di}'
+                        activities[cnt:cnt+m] = activity
                         labels[cnt:cnt+m] = gait_label
 
                         cnt += m  # increment count
 
-    return dataset, subjects, labels
+    return dataset, labels, subjects, activities
 
 
 
