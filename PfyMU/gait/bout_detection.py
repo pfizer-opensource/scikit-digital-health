7da7eb3e93cff73fd022b1163221a3921fc68098
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-08-31 10:36:01 -0400

    add classifier use, other testing

--- PfyMU/gait/bout_detection.py
+++ PfyMU/gait/bout_detection.py
@@ -6,10 +6,86 @@ Pfizer DMTI 2020
 """
 from numpy import mean, diff, round
 from scipy.signal import butter, sosfiltfilt
+from scipy.interpolate import interp1d
+from warnings import warn
+from pathlib import Path
+from importlib import resources
+import lightgbm as lgb
 
 
 from PfyMU.base import _BaseProcess
 from PfyMU.utility import get_windowed_view
+from PfyMU.features import Bank
+
+
+def get_lgb_gait_classification(accel, fs)
+    """
+    Get classification of windows of accelerometer data
+
+    Paramters
+    ---------
+    accel : numpy.ndarray
+        (N, 3) array of acceleration values, in units of "g"
+    fs : float
+        Sampling frequency of the data
+    """
+    goal_fs = 50.0  # goal fs for classifier
+    wlen = int(goal_fs * 3)
+    step = int(0.75 * wlen)
+    thresh = 0.7  # mean + 1 standard deviation of best threshold for maximizing F1 score
+
+    # down/up sample if necessary
+    if (fs != goal_fs):
+        if (fs < goal_fs):
+            warn(f"fs ({fs:.2f}) is less than 50.0Hz. Upsampling to 50.0Hz")
+        
+        f_rs = interp1d(np.arange(0, accel.shape[0]) / fs, 1 / fs, accel, axis=0, bounds_error=False, fill_value='extrapolate')
+
+        accel_rs = f_rs(np.arange(0, accel.shape[0] / fs, 1 / goal_fs))
+    else:
+        accel_rs = accel
+    
+    # band-pass filter
+    sos = butter(1, [2 * 0.25 / fs, 2 * 5 / fs], btype='band', output='sos')
+    accel_rs_f = sosfiltfilt(sos, np.linalg.norm(accel_rs, axis=1))
+
+    # window
+    accel_w = get_windowed_view(accel_rs_f, wlen, wstep, ensure_c_contiguity=True)
+
+    # get the feature bank
+    feat_bank = Bank(window_length=None, window_step=None)  # make sure no windowing
+    with resources.path('PfyMU.gait.models', 'final_features.json') as file_path:
+        feat_bank.load(file_path)
+    
+    # compute the features
+    accel_feats = feat_bank.compute(accel_w, fs=goal_fs, windowed=True)
+
+    # load the model
+    with resources.path('PfyMU.gait.models', 'lgbm_gait_classifier_no-stairs.lgbm') as file_path:
+        bst = lgb.Booster(model_file=file_path)
+    
+    # predict
+    gait_predictions = bst.predict(accel_feats)
+    
+    # expand the predictions to be per sample.
+    tmp = np.zeros(accel.shape[0])
+
+    c = np.zeros(accel.shape[0], dtype='int')  # keep track of overlap
+    for i, p in enumerate(gait_predictions):
+        i1 = i * wstep
+        i2 = i1 + wlen
+        # add prediction and keep track of counts effecting that cell to deal with
+        # overlapping windows and therefore overlapping predictions
+        tmp[i1:i2] += p
+        c[i1:i2] += 1
+    # make sure no divide by 0
+    c[i2:] = 1
+
+    tmp /= c
+    gait_pred_ps = tmp >= 0.5  # final gait predictions
+    return gait_pred_ps
+
+
 
 
 class ThresholdGaitDetection(_BaseProcess):
