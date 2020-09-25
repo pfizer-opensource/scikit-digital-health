8d169bf8a1027dbfff28cb14fb9ef656661a9231
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-09-25 09:09:28 -0400

    Fix conftest for new test location

--- test/sit2stand/conftest.py
+++ test/sit2stand/conftest.py
@@ -1,36 +1,21 @@
 from pytest import fixture
 import h5py
-from sys import version_info
-
-if version_info >= (3, 7):
-    from importlib import resources
-else:
-    import importlib_resources
+from pathlib import Path
 
 
 # TRUTH DATA
 @fixture(scope='module')
 def truth_path():
-    if version_info >= (3, 7):
-        with resources.path('PfyMU.sit2stand.tests.data', 'sample.h5') as p:
-            path = p
-    else:
-        with importlib_resources.path('PfyMU.sit2stand.tests.data', 'sample.h5') as p:
-            path = p
+    path = Path('../data/sample.h5')
     return path
 
 
 # RAW DATA
 @fixture(scope='module')
 def sample_data():
-    if version_info >= (3, 7):
-        with resources.path('PfyMU.sit2stand.tests.data', 'sample.h5') as p:
-            with h5py.File(p, 'r') as f:
-                accel = f['Sensors']['Lumbar']['Accelerometer'][()] / 9.81
-                time = f['Sensors']['Lumbar']['Unix Time'][()]
-    else:
-        with importlib_resources.path('PfyMU.sit2stand.tests.data', 'sample.h5') as p:
-            with h5py.File(p, 'r') as f:
-                accel = f['Sensors']['Lumbar']['Accelerometer'][()] / 9.81
-                time = f['Sensors']['Lumbar']['Unix Time'][()]
+    path = Path('../data/sample.h5')
+    with h5py.File(path, 'r') as f:
+        accel = f['Sensors']['Lumbar']['Accelerometer'][()] / 9.81
+        time = f['Sensors']['Lumbar']['Unix Time'][()]
+
     return {'time': time, 'accel': accel}
