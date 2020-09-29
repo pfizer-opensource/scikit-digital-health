78684183ed8dad29f466c93262b15821419fc4c7
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-09-29 12:34:29 -0400

    Add methods and __all__

---
+++ src/PfyMU/read/__init__.py
@@ -0,0 +1,6 @@
+from PfyMU.read.axivity import ReadCWA
+from PfyMU.read import axivity
+from PfyMU.read.geneactiv import ReadBin
+from PfyMU.read import geneactiv
+
+__all__ = ('ReadCWA', 'ReadBin', 'axivity', 'geneactiv')
