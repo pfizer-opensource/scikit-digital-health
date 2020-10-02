1ac0acc7a7510a2f8ae54771ce76c32b1e1c8ecd
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-02 12:55:08 -0400

    Add autocreation of a template module

---
+++ templates/autocreate_module.py
@@ -0,0 +1,54 @@
+"""
+Automatically create a base module that can be modified
+
+Lukas Adamowicz
+2020, Pfizer DMTI
+"""
+from datetime import datetime
+from pathlib import Path
+
+
+def file_replace(lines, name, replace_with):
+    for i, line in enumerate(lines):
+        if '!{{ ' + name + ' }}' in line:
+            lines[i] = line.replace('!{{ ' + name + ' }}', replace_with)
+
+    return lines
+
+
+if __name__ == "__main__":
+    module_name = input("Module name: ")
+    author = input("Author: ")
+    short_descr = input("Short description: ")
+    year = datetime.now().year
+
+    if not module_name[0].isupper():
+        module_name = module_name.capitalize()
+
+    if module_name == '' or author == '' or short_descr == '':
+        raise ValueError('please provide module name, author, and a short description')
+
+    with open('templates/core_template.py', 'r') as f:
+        lines = f.readlines()
+
+    file_replace(lines, 'module_name', module_name)
+    file_replace(lines, 'author', author)
+    file_replace(lines, 'short_description', short_descr)
+    file_replace(lines, 'year', year)
+
+    folder = Path('src/PfyMU') / module_name.lower()
+
+    folder.mkdir()
+
+    core_file = folder / f'{module_name.lower()}.py'
+    with open(core_file, 'w') as f:
+        f.writelines(lines)
+
+    init_file = folder / '__init__.py'
+    with open(init_file, 'w') as f:
+        f.write(
+            f"""
+            from PfyMU.{module_name.lower()}.{module_name.lower()} import {module_name}
+            
+            """
+        )
