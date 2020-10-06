a4c15aa2ed1d4a4befc8b03878f3abdf7c99a5e2
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-06 15:49:24 -0400

    remove sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ release = PfyMU.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'numpydoc']  #, 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
