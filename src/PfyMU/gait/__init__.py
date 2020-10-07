a4e5bd9761b479280c05089747b489c3a8ff5a21
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-07 09:09:42 -0400

    Adding documentation for the gait module

--- src/PfyMU/gait/__init__.py
+++ src/PfyMU/gait/__init__.py
@@ -1,3 +1,28 @@
+"""
+IMU Gait Analysis (:mod:`PfyMU.gait`)
+====================================
+
+.. currentmodule:: PfyMU.gait
+
+Pipeline gait processing
+------------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    Gait
+
+Gait Metrics
+------------
+
+.. autosummary::
+    :toctree: generated/
+
+    StrideTime
+    StanceTime
+    SwingTime
+    StepTime
+"""
 from PfyMU.gait.gait import Gait
 from PfyMU.gait import gait
 from PfyMU.gait.gait_metrics import *
