47ce7166fcab0e8eff38ec6a5e86af7262edec82
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-09 11:19:06 -0400

    make import paths absolute

--- src/skimu/features/lib/_cython/__init__.py
+++ src/skimu/features/lib/_cython/__init__.py
@@ -1,11 +1,11 @@
-from .autocorrelation import Autocorrelation
-from .complexityInvariantDistance import CID
-from .correlationCoefficient import CC3
-from .entropy import SignalEntropy, SampleEntropy
-from .frequencyFeatures import FrequencyFeatures
-from .jerk import JerkMetric, DimensionlessJerk
-from .linearSlope import LinRegression
-from .rangeCount import RangeCount
-from .ratioBeyondRSigma import RatioBeyondRSigma
-from .rootMeanVariance import RootMeanVariance
-from .sparc import SPARC
+from skimu.features.lib._cython.autocorrelation import Autocorrelation
+from skimu.features.lib._cython.complexityInvariantDistance import CID
+from skimu.features.lib._cython.correlationCoefficient import CC3
+from skimu.features.lib._cython.entropy import SignalEntropy, SampleEntropy
+from skimu.features.lib._cython.frequencyFeatures import FrequencyFeatures
+from skimu.features.lib._cython.jerk import JerkMetric, DimensionlessJerk
+from skimu.features.lib._cython.linearSlope import LinRegression
+from skimu.features.lib._cython.rangeCount import RangeCount
+from skimu.features.lib._cython.ratioBeyondRSigma import RatioBeyondRSigma
+from skimu.features.lib._cython.rootMeanVariance import RootMeanVariance
+from skimu.features.lib._cython.sparc import SPARC
