fe66e72e599a0ff0d8c76e3dcf6f1f9e00edcefc
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-09 08:15:21 -0400

    Add documentation for the module

--- src/PfyMU/features/__init__.py
+++ src/PfyMU/features/__init__.py
@@ -1,3 +1,59 @@
+"""
+Signal Features (:mod:`PfyMU.features`)
+====================================
+
+.. currentmodule:: PfyMU.features
+
+Combined Feature Processing
+---------------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    Bank
+
+Features
+--------
+
+.. autosummary::
+    :toctree: generated/
+
+    Mean
+    MeanCrossRate
+    StdDev
+    Skewness
+    Kurtosis
+    Range
+    IQR
+    RMS
+    Autocorrelation
+    LinearSlope
+    SignalEntropy
+    SampleEntropy
+    PermutationEntropy
+    DominantFrequency
+    DominantFrequencyValue
+    PowerSpectralSum
+    SpectralFlatness
+    SpectralEntropy
+    ComplexityInvariantDistance
+    RangeCountPercentage
+    RatioBeyondRSigma
+    JerkMetric
+    DimensionlessJerk
+    SPARC
+    DetailPower
+    DetailPowerRatio
+
+Utility Functions
+-----------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    compute_window_samples
+    get_windowed_view
+"""
 from PfyMU.features.core import *
 from PfyMU.features import core
 from PfyMU.features.utility import *
