d32aa6c9f28a38c235e17754b5a455f28cc0f599
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-09 12:37:09 -0400

    update numpy intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ epub_exclude_files = ['search.html']
 # -----------------------------------------------------------------------------
 intersphinx_mapping = {
     'python': ('https://docs.python.org/dev', None),
-    'numpy': ('https://numpy.org/doc/stable/reference/index.html', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
 }
