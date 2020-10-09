65e451ebaffb3fb26fc0f8077f9490828e0cdfe5
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-09 14:44:21 -0400

    update compute docstrings

--- src/skimu/features/lib/misc.py
+++ src/skimu/features/lib/misc.py
@@ -18,16 +18,34 @@ class ComplexityInvariantDistance(Feature):
     ----------
     normalize : bool, optional
         Normalize the signal. Default is True.
-
-    Methods
-    -------
-    compute(signal[, columns=None, windowed=False])
     """
     def __init__(self, normalize=True):
         super(ComplexityInvariantDistance, self).__init__(
             'ComplexityInvariantDistance', {'normalize': normalize}
         )
         self.normalize = normalize
+
+    def compute(self, *args, **kwargs):
+        """
+        compute(signal, *, columns=None, windowed=False)
+
+        Compute the complexity invariant distance
+
+        Parameters
+        ----------
+        signal : {numpy.ndarray, pandas.DataFrame}
+            Either a numpy array (up to 3D) or a pandas dataframe containing the signal
+        columns : array_like, optional
+            Columns to use if signal is a pandas.DataFrame. If None, uses all columns.
+        windowed : bool, optional
+            If the signal has already been windowed. Default is False.
+
+        Returns
+        -------
+        cid : {numpy.ndarray, pandas.DataFrame}
+            Computed complexity invariant distance, returned as the same type as the input signal
+        """
+        return super().compute(*args, **kwargs)
 
     def _compute(self, x, fs):
         super(ComplexityInvariantDistance, self)._compute(x, fs)
@@ -44,10 +62,6 @@ class RangeCountPercentage(Feature):
         Minimum value of the range. Default value is -1.0
     range_max : {int, float}, optional
         Maximum value of the range. Default value is 1.0
-
-    Methods
-    -------
-    compute(signal[, columns=None, windowed=False])
     """
     def __init__(self, range_min=-1.0, range_max=1.0):
         super(RangeCountPercentage, self).__init__(
@@ -57,6 +71,28 @@ class RangeCountPercentage(Feature):
 
         self.rmin = range_min
         self.rmax = range_max
+
+    def compute(self, *args, **kwargs):
+        """
+        compute(signal, *, columns=None, windowed=False)
+
+        Compute the range count percentage
+
+        Parameters
+        ----------
+        signal : {numpy.ndarray, pandas.DataFrame}
+            Either a numpy array (up to 3D) or a pandas dataframe containing the signal
+        columns : array_like, optional
+            Columns to use if signal is a pandas.DataFrame. If None, uses all columns.
+        windowed : bool, optional
+            If the signal has already been windowed. Default is False.
+
+        Returns
+        -------
+        rcp : {numpy.ndarray, pandas.DataFrame}
+            Computed range count percentage, returned as the same type as the input signal
+        """
+        return super().compute(*args, **kwargs)
 
     def _compute(self, x, fs):
         super(RangeCountPercentage, self)._compute(x, fs)
@@ -72,10 +108,6 @@ class RatioBeyondRSigma(Feature):
     ----------
     r : float, optional
         Number of standard deviations above or below the mean the range includes. Default is 2.0
-
-    Methods
-    -------
-    compute(signal[, columns=None, windowed=False])
     """
     def __init__(self, r=2.0):
         super(RatioBeyondRSigma, self).__init__(
@@ -84,6 +116,28 @@ class RatioBeyondRSigma(Feature):
         )
 
         self.r = r
+
+    def compute(self, *args, **kwargs):
+        r"""
+        compute(signal, *, columns=None, windowed=False)
+
+        Compute the ratio beyond :math:`r\sigma`
+
+        Parameters
+        ----------
+        signal : {numpy.ndarray, pandas.DataFrame}
+            Either a numpy array (up to 3D) or a pandas dataframe containing the signal
+        columns : array_like, optional
+            Columns to use if signal is a pandas.DataFrame. If None, uses all columns.
+        windowed : bool, optional
+            If the signal has already been windowed. Default is False.
+
+        Returns
+        -------
+        rbr : {numpy.ndarray, pandas.DataFrame}
+            Computed ratio beyond r sigma, returned as the same type as the input signal
+        """
+        return super().compute(*args, **kwargs)
 
     def _compute(self, x, fs):
         super(RatioBeyondRSigma, self)._compute(x, fs)
