d2e0ab380966469b29bccf91a54205e5f9aaa382
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-16 11:42:57 -0400

    Add error/case tests for feature bank

--- test/features/test_feature_bank.py
+++ test/features/test_feature_bank.py
@@ -1,3 +1,5 @@
+from tempfile import NamedTemporaryFile
+
 import pytest
 from numpy import allclose, random
 import pandas as pd
@@ -41,6 +43,22 @@ class TestBank:
         # check that the bank uses the same instance of the Range computation
         assert feat_bank._feat_list[1].parent is feat_bank._feat_list[3].parent  # check same instance
 
+    def test_save_load(self, fs, acc, bank_2d_truth):
+        feat_bank = self.set_up()
+
+        ntf = NamedTemporaryFile('r+')
+
+        feat_bank.save(ntf.name)
+
+        feat_bank = None
+        feat_bank = Bank(None, None)
+        feat_bank.load(ntf.name)
+        ntf.close()
+
+        pred = feat_bank.compute(acc, 20.0)
+
+        assert allclose(pred, bank_2d_truth)
+
     def test_no_features_error(self):
         feat_bank = Bank(window_length=None, window_step=None)
 
@@ -52,4 +70,11 @@ class TestBank:
 
         with pytest.raises(ValueError):
             feat_bank.compute(random.rand(150, 10, 3), 50.0, ['x', 'y'], False)
+
+    @pytest.mark.parametrize('non_feat', (lambda x: x**2, list(), {}, 5))
+    def test_non_feature_add_error(self, non_feat):
+        feat_bank = Bank(None, None)
+
+        with pytest.raises(NotAFeatureError):
+            feat_bank + non_feat
 
