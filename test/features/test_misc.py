5a01a332bcd226d58540af3a09d682b53c15e918
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-19 09:28:33 -0400

    Add error testing for get_windowed_view

--- test/features/test_misc.py
+++ test/features/test_misc.py
@@ -1,13 +1,51 @@
 import inspect
 
 import pytest
-from numpy import random
+from numpy import random, asfortranarray
 import pandas as pd
 
 from skimu.features import *
 from skimu.features import lib
 from skimu.features.core import DeferredFeature, NotAFeatureError
 from skimu.features.utility import standardize_signal, DimensionError, InputTypeError
+
+
+class TestGetWindowedView:
+    def test_ndim_error(self):
+        with pytest.raises(DimensionError):
+            get_windowed_view(random.rand(5, 3, 4, 1), 5, 5)
+
+    def test_c_cont_error(self):
+        with pytest.raises(ValueError):
+            get_windowed_view(
+                asfortranarray(random.rand(10, 3)),
+                3, 3
+            )
+
+
+class TestComputeWindowSamples:
+    @pytest.mark.parametrize(
+        ('fs', 'L', 'S', 'res_l', 'res_s'),
+        (
+                (50.0, 3.0, 1.0, 150, 150),
+                (50.0, 1.5, 300, 75, 300),
+                (100.0, 2.5, 0.5, 250, 125),
+                (10.0, 3.0, 0.0001, 30, 1)
+        )
+    )
+    def test(self, fs, L, S, res_l, res_s):
+        nl, ns = compute_window_samples(fs, L, S)
+
+        assert nl == res_l
+        assert ns == res_s
+
+    def test_negative_step_error(self):
+        with pytest.raises(ValueError):
+            compute_window_samples(50.0, 3.0, -10)
+
+    def test_float_error(self):
+        with pytest.raises(ValueError):
+            compute_window_samples(50.0, 3.0, 1.1)
 
 
 class TestStandardizeSignal:
