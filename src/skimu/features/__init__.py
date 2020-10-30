e4c1aee88ee3f6e29ff9db2495b530e7c0b71645
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-30 08:52:21 -0400

    remove utility functions no longer present

--- src/skimu/features/__init__.py
+++ src/skimu/features/__init__.py
@@ -44,20 +44,9 @@ Features
     SPARC
     DetailPower
     DetailPowerRatio
-
-Utility Functions
------------------
-
-.. autosummary::
-    :toctree: generated/
-
-    compute_window_samples
-    get_windowed_view
 """
 from skimu.features.core import *
 from skimu.features import core
-from skimu.features.utility import *
-from skimu.features import utility
 from skimu.features.lib import *
 from skimu.features import lib
 
