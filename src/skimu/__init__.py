0998871fa2ef07ea445d3fbb31e12eb53ad67ba6
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-30 08:52:36 -0400

    Start updating documentation for scikit-imu general features

--- src/skimu/__init__.py
+++ src/skimu/__init__.py
@@ -1,10 +1,26 @@
+"""
+Scikit IMU (:mod:`skimu`)
+=======================================
+
+.. currentmodule:: skimu
+
+Utility Functions
+-----------------
+
+.. autosummary::
+    :toctree: generated/
+
+    utility.compute_window_samples
+    utility.get_windowed_view
+"""
 from skimu.version import __version__
 
 from skimu.pipeline import Pipeline
+from skimu import utility
 
 from skimu import gait
 from skimu import sit2stand
 from skimu import features
 
 __skimu_version__ = __version__
-__all__ = ['gait', 'sit2stand', 'features', '__skimu_version__']
+__all__ = ['Pipeline', 'gait', 'sit2stand', 'features', 'utility', '__skimu_version__']
