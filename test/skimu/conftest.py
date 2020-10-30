a4d2fa7afa1f4beaca6b62744938fe408339ff94
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-30 10:54:19 -0400

    Add files for testing saving pipelines etc

---
+++ test/skimu/conftest.py
@@ -0,0 +1,21 @@
+from tempfile import NamedTemporaryFile
+
+from pytest import fixture
+
+
+@fixture(scope='module')
+def pipeline_file():
+    ntf = NamedTemporaryFile(mode='a')
+
+    yield ntf.name
+
+    ntf.close()
+
+
+@fixture(scope='module')
+def gait_res_file():
+    ntf = NamedTemporaryFile(mode='a')
+
+    yield ntf.name
+
+    ntf.close()
