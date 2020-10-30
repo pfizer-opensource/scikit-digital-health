e81a1a71dbe63c4ffd0955fba3294a9ebd5196e5
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-10-30 11:03:48 -0400

    fix expected names

--- test/skimu/test_base_process.py
+++ test/skimu/test_base_process.py
@@ -5,11 +5,11 @@ from skimu.base import _BaseProcess
 
 class TestBaseProcess:
     def test_init_repr_str(self):
-        bp = _BaseProcess('Process', True, kw1=None, kw2=0, kw3=0.5, kw4='str', kw5=False)
+        bp = _BaseProcess(True, kw1=None, kw2=0, kw3=0.5, kw4='str', kw5=False)
 
-        r = "Process(kw1=None, kw2=0, kw3=0.5, kw4='str', kw5=False)"
+        r = "_BaseProcess(kw1=None, kw2=0, kw3=0.5, kw4='str', kw5=False)"
 
-        assert bp._name == "Process"
+        assert bp._name == "_BaseProcess"
         assert bp._return_result
         assert repr(bp) == r
-        assert str(bp) == "Process"
+        assert str(bp) == "_BaseProcess"
