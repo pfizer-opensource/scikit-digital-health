8e9bd2505b5811526ac7694f815aecd2b1d22f2c
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-11-10 15:18:45 -0500

    remove old _return_results arg

--- test/skimu/test_base_process.py
+++ test/skimu/test_base_process.py
@@ -10,6 +10,5 @@ class TestBaseProcess:
         r = "_BaseProcess(kw1=None, kw2=0, kw3=0.5, kw4='str', kw5=False)"
 
         assert bp._name == "_BaseProcess"
-        assert bp._return_result
         assert repr(bp) == r
         assert str(bp) == "_BaseProcess"
