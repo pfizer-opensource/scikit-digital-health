865a801f1cb13cbea7a4e451b212a10769beb244
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-11-10 08:18:56 -0500

    Add enable logging to base process class

--- src/skimu/base.py
+++ src/skimu/base.py
@@ -5,6 +5,8 @@ Lukas Adamowicz
 Pfizer DMTI 2020
 """
 from datetime import date as dt_date
+import logging
+
 from pandas import DataFrame
 
 
@@ -46,6 +48,9 @@ class _BaseProcess:
 
         self._kw = kwargs
 
+        self.logger = None
+        self.log_filename = None
+
     def predict(self, *args, **kwargs):
         result = self._predict(*args, **kwargs)
 
@@ -53,6 +58,38 @@ class _BaseProcess:
             return result[1]
         else:
             return result[0]
+
+    def enable_logging(self, logger=None, filename='pipeline_log.log', name=None):
+        """
+        Enable logging during the execution of the pipeline. This will initialize loggers
+        for steps already added, and any new steps that are added to the pipeline.
+
+        Parameters
+        ----------
+        logger : logging.Logger, optional
+            Custom logger for logging. If None (default), a logger will be created. See Notes for
+            a more detailed description of the logger that will be created.
+        filename : str, optional
+            Filename/path for the logger. Default is 'pipeline_log.log'.
+        name : str, optional
+            Name for the logger. Default is None, which will use __name__ (skimu.pipeline)
+
+        Notes
+        -----
+        If a logger is being created, it is created with the following attributes/parameters:
+
+        - Name is set to __name__ (`name=None`) or `name`
+        - Level is set to `INFO`
+        - Handler is a `logging.FileHandler` with `filename` for the file
+        """
+        if logger is None:
+            self.log_filename = filename  # save the file name
+            self.logger = logging.getLogger(name if name is not None else __name__)
+            self.logger.setLevel('INFO')  # set the level for the logger
+            self.logger.addHandler(logging.FileHandler(filename=filename))
+        else:
+            if isinstance(logger, logging.Logger):
+                self.logger = logger
 
     def save_results(self, results, file_name):
         """
