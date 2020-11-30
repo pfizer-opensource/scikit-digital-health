76023f968fe3624eece2b0bd994c2fdb7aed900e
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-11-30 12:52:43 -0500

    Add actigraph/gt3x to inits

--- src/skimu/read/_extensions/__init__.py
+++ src/skimu/read/_extensions/__init__.py
@@ -1,4 +1,5 @@
 from .bin_convert import bin_convert
 from .cwa_convert import read_cwa
+from .read_gt3x import read_gt3x
 
-__all__ = ('bin_convert', 'read_cwa')
+__all__ = ('bin_convert', 'read_cwa', 'read_gt3x')
