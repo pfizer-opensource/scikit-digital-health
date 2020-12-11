194681d280050b81f7ac4d69189fdab1e3807329
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-12-11 10:50:12 -0500

    Update gait data for changed scale calculation

--- test/gait/test_gait.py
+++ test/gait/test_gait.py
@@ -127,7 +127,7 @@ class TestGetGaitStrides:
         gait = {i: [] for i in keys}
         bout_steps = get_strides(gait, accel[:, axis], 0, ic, fc, time, 50.0, 2.25, 0.2)
 
-        assert bout_steps == 45
+        assert bout_steps == 42
         for k in keys:
             assert allclose(gait[k], gait_truth[k], equal_nan=True)
 
