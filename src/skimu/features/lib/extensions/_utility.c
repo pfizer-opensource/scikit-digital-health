1821dbed474f92ba8ef916592151dca15c429fa1
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-12-18 08:17:39 -0500

    Add cf_unique interface

--- src/skimu/features/lib/extensions/_utility.c
+++ src/skimu/features/lib/extensions/_utility.c
@@ -14,11 +14,11 @@ extern void quick_argsort_(long *, double *, long *);
 extern void quick_sort_(long *, double *);
 
 
-PyObject * c_mean_sd_1d(PyObject *NPY_UNUSED(self), PyObject *args){
+PyObject * cf_mean_sd_1d(PyObject *NPY_UNUSED(self), PyObject *args){
     PyObject *x_;
     int fail;
 
-    if (!PyArg_ParseTuple(args, "O:c_mean_sd_1d", &x_)) return NULL;
+    if (!PyArg_ParseTuple(args, "O:cf_mean_sd_1d", &x_)) return NULL;
 
     PyArrayObject *data = (PyArrayObject *)PyArray_FromAny(
         x_, PyArray_DescrFromType(NPY_DOUBLE), 1, 0,
@@ -49,9 +49,52 @@ PyObject * c_mean_sd_1d(PyObject *NPY_UNUSED(self), PyObject *args){
     )
 }
 
+PyObject * cf_unique(PyObject *NPY_UNUSED(self), PyObject *args){
+    PyObject *x_;
+    int fail;
+
+    if (!PyArg_ParseTuple(args, "O:cf_unique", &x_)) return NULL;
+
+    PyArrayObject *data = (PyArrayObject *)PyArray_FromAny(
+        x_, PyArray_DescrFromType(NPY_DOUBLE), 1, 0,
+        NPY_ARRAY_ENSUREARRAY | NPY_ARRAY_CARRAY_RO, NULL
+    );
+    if (!data) return NULL;
+
+    int ndim = PyArray_NDIM(data);
+
+    if (ndim != 1){
+        PyExc_ValueError("Number of dimensions cannot be other than 1.")
+        Py_XDECREF(data);
+        return NULL;
+    }
+    npy_intp *ddims = PyArray_DIMS(data);
+
+    PyArrayObject *unq = (PyArrayObject *)PyArray_ZEROS(1, ddims, NPY_DOUBLE, 0);
+    PyArrayObject *cnt = (PyArrayObject *)PyArray_ZEROS(1, ddims, NPY_LONG, 0);
+
+    double mean = 0., stdev = 0.;
+    double *dptr = (double *)PyArray_DATA(data);
+    double *uptr = (double *)PyArray_DATA(unq);
+    double *cptr = (double *)PyArray_DATA(cnt);
+    long n_unique = 0;
+
+    unique(&ddims[0], dptr, uptr, cptr, &n_unique);
+
+    Py_XDECREF(data);
+
+    return Py_BuildValue(
+        "OOl",
+        PyFloat_FromDouble(unq),
+        PyFloat_FromDouble(cnt),
+        PyFloat_FromDouble(n_unique)
+    )
+}
+
 
 static struct PyMethodDef methods[] = {
-    {"c_mean_sd_1d",   permutation_entropy,   1, NULL},  // last is test__doc__
+    {"cf_mean_sd_1d",   permutation_entropy,   1, NULL},  // last is test__doc__
+    {"cf_unique",   cf_unique,   1, NULL},
     {NULL, NULL, 0, NULL}          /* sentinel */
 };
 
