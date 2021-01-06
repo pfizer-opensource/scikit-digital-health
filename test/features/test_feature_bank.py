ea366d4e5f12881e3e0027f035671aedf6af12b9
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-01-06 10:36:26 -0500

    Add tests to cover feature bank code

--- test/features/test_feature_bank.py
+++ test/features/test_feature_bank.py
@@ -5,7 +5,7 @@ from numpy import allclose, random
 import pandas as pd
 
 from skimu.features import *
-from skimu.features.core import NoFeaturesError, NotAFeatureError
+from skimu.features.core import ArrayConversionError, NotAFeatureError
 
 
 class TestBank:
@@ -41,6 +41,42 @@ class TestBank:
         # check that values match truth values (ie doing correct computations)
         assert allclose(pred, bank_2d_truth)
 
+    def test_datafram_2cols(self, fs, acc, bank_2d_truth):
+        feat_bank = Bank()
+        feat_bank.add([
+            Mean(),
+            Range()[0],
+            JerkMetric(),
+            Range()[1]
+        ])
+        mask = [True, True, False, True, False, True, True, False, True]
+
+        acc_df = pd.DataFrame(data=acc, columns=['x', 'y', 'z'])
+
+        _ = feat_bank.compute(acc_df, 20.0)
+        pred, cols = feat_bank.compute(acc_df, 20.0, columns=['x', 'y'])
+
+        # check that values match truth values (ie doing correct computations)
+        assert allclose(pred, bank_2d_truth[0, mask])
+
+    def test_slice_index(self):
+        feat_bank = Bank()
+        feat_bank.add([
+            Mean()[0:4:2]
+        ])
+
+        x = random.random((4, 50))
+
+        res = feat_bank.compute(x)
+
+        assert res.shape == (2,)
+
+    def test_ragged_list_input_error(self):
+        feat_bank = self.set_up()
+
+        with pytest.raises(ArrayConversionError):
+            feat_bank.compute([[1], [1, 2], [1, 2, 3]], 20.)
+
     def test_save_load(self, fs, acc, bank_2d_truth):
         feat_bank = self.set_up()
 
@@ -57,11 +93,22 @@ class TestBank:
 
         assert allclose(pred, bank_2d_truth)
 
-    # def test_no_features_error(self):
-    #     feat_bank = Bank()
-    #
-    #     with pytest.raises(NoFeaturesError):
-    #         feat_bank.compute(None, None)
+    def test_len(self):
+        feat_bank = self.set_up()
+
+        assert len(feat_bank) == 4
+
+    def test_add_duplicate(self):
+        feat_bank = self.set_up()
+
+        with pytest.warns(UserWarning):
+            feat_bank.add(Mean())
+
+    def test_add_duplicate_in_list(self):
+        feat_bank = self.set_up()
+
+        with pytest.warns(UserWarning):
+            feat_bank.add([Mean(), DominantFrequency(), Range()[1]])
 
     def test_columns_size_error(self):
         feat_bank = self.set_up()
@@ -75,4 +122,10 @@ class TestBank:
 
         with pytest.raises(NotAFeatureError):
             feat_bank.add(non_feat)
+
+    def test_non_feature_add_in_list_error(self):
+        feat_bank = Bank()
+
+        with pytest.raises(NotAFeatureError):
+            feat_bank.add([Mean(), DominantFrequency(), 5])
 
