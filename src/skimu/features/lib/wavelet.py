b78ce98d1290ca12695d2d992c77f32419317695
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-01-08 10:25:04 -0500

    add __slots__

--- src/skimu/features/lib/wavelet.py
+++ src/skimu/features/lib/wavelet.py
@@ -29,6 +29,7 @@ class DetailPower(Feature):
     .. [1] Sekine, M. et al. "Classification of waist-acceleration signals in a continuous
         walking record." Medical Engineering & Physics. Vol. 22. Pp 285-291. 2000.
     """
+    __slots__ = ("wave", "f_band")
     _wavelet_options = pywt.wavelist(kind='discrete')
 
     def __init__(self, wavelet='coif4', freq_band=None):
@@ -117,6 +118,7 @@ class DetailPowerRatio(Feature):
     .. [1] Sekine, M. et al. "Classification of waist-acceleration signals in a continuous
         walking record." Medical Engineering & Physics. Vol. 22. Pp 285-291. 2000.
     """
+    __slots__ = ("wave", "f_band")
     _wavelet_options = pywt.wavelist(kind='discrete')
 
     def __init__(self, wavelet='coif4', freq_band=None):
