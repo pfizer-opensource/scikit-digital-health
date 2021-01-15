c3ed816175124c99e06092f240238aca4d8afa9e
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-01-15 12:34:51 -0500

    Move imports around and import new preprocessing module

--- src/skimu/__init__.py
+++ src/skimu/__init__.py
@@ -24,12 +24,13 @@ Utility Functions
 from skimu.version import __version__
 
 from skimu.pipeline import Pipeline
-from skimu import utility
 
+from skimu import utility
+from skimu import read
+from skimu import preprocessing
 from skimu import gait
 from skimu import sit2stand
 from skimu import features
-from skimu import read
 
 __skimu_version__ = __version__
 __all__ = ['Pipeline', 'gait', 'sit2stand', 'read', 'features', 'utility', '__skimu_version__']
