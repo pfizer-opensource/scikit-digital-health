Fix GaitSymmetryIndex calculation to use the proper point in the autocorrelation, and account for negative values

src/skimu/gait/gait_metrics/gait_metrics.py
@@ -30,7 +30,7 @@ stride length: step_length_i + step_length_i+1
 gait speed: stride_length / stride time
 """
 from numpy import zeros, nanmean, mean, nanstd, std, sum, sqrt, nan, nonzero, argmin, abs, round, \
-    float_, int_, fft, arange, isnan
+    float_, int_, fft, arange, isnan, maximum
 from numpy.linalg import norm
 from scipy.signal import butter, sosfiltfilt, find_peaks
 
@@ -616,9 +616,11 @@ class GaitSymmetryIndex(BoutMetric):
             # C_stride is the sum of 3 axes
             pks, _ = find_peaks(sum(ac, axis=1))
             # find the closest peak to the computed ideal half stride lag
-            idx = int(0.5 * argmin(abs(pks - lag)))
+            t_stride = pks[argmin(abs(pks - lag))]
+            idx = int(0.5 * t_stride)
 
-            gsi[i] = sqrt(sum(ac[idx])) / sqrt(3)
+            # maximum ensures no sqrt of negative numbers
+            gsi[i] = sqrt(sum(maximum(ac[idx], 0))) / sqrt(3)
 
         gait[self.k_] = gsi[gait_aux['inertial data i']]
 