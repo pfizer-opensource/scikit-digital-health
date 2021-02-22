d9b076cbdadd930c0350ebcc4bcf767d60188393
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-02-22 08:57:16 -0500

    Add hfen metrics

--- src/skimu/activity/metrics.py
+++ src/skimu/activity/metrics.py
@@ -64,7 +64,56 @@ def metric_bfen(accel, fs, low_cutoff=0.2, high_cutoff=15):
     Returns
     -------
     bfen : numpy.ndarray
-        (N, ) array of band-pass filtered and euclidean normed accelerations
+        (N, ) array of band-pass filtered and euclidean normed accelerations.
     """
     sos = butter(4, [2 * low_cutoff / fs, 2 * high_cutoff / fs], btype='bandpass', output='sos')
     return norm(sosfiltfilt(sos, accel, axis=0), axis=1)
+
+
+def metric_hfen(accel, fs, low_cutoff=0.2):
+    """
+    High-pass filtered euclidean norm.
+
+    Parameters
+    ----------
+    accel : numpy.ndarray
+        (N, 3) array of acceleration values in g.
+    fs : float
+        Sampling frequency of `accel` in Hz.
+    low_cutoff : float, optional
+        High-pass cutoff in Hz. Default is 0.2Hz.
+
+    Returns
+    -------
+    hfen : numpy.ndarray
+        (N, ) array of high-pass filtered and euclidean normed accelerations.
+    """
+    sos = butter(4, 2 * low_cutoff / fs, btype='high', output='sos')
+    return norm(sosfiltfilt(sos, accel, axis=0), axis=1)
+
+
+def metric_hfenplus(accel, fs, cutoff=0.2):
+    """
+    High-pass filtered euclidean norm plus the low-pass filtered euclidean norm minus 1g.
+
+    Parameters
+    ----------
+    accel : numpy.ndarray
+        (N, 3) array of acceleration values in g.
+    fs : float
+        Sampling frequency of `accel` in Hz.
+    cutoff : float, optional
+        Cutoff in Hz for both high and low filters. Default is 0.2Hz.
+
+    Returns
+    -------
+    hfenp : numpy.ndarray
+        (N, ) array of high-pass filtered acceleration norm added to the low-pass filtered
+        norm minus 1g.
+    """
+    sos_low = butter(4, 2 * cutoff / fs, btype="low", output="sos")
+    sos_high = butter(4, 2 * cutoff / fs, btype="high", output="sos")
+
+    acc_high = norm(sosfiltfilt(sos_high, accel, axis=0), axis=1)
+    acc_low = norm(sosfiltfilt(sos_low, accel, axis=0), axis=1)
+    return acc_high + acc_low - 1
