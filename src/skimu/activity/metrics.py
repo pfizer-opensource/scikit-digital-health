7c8c2cdb5e085a0eda6b5605efba4285b6b1634b
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-02-23 08:56:23 -0500

    Modified metrics to return the rolling mean/per window values

--- src/skimu/activity/metrics.py
+++ src/skimu/activity/metrics.py
@@ -4,12 +4,17 @@ Metrics for classifying activity
 Lukas Adamowicz
 Pfizer DMTI 2021
 """
-from numpy import minimum, abs
+from numpy import minimum, abs, repeat
 from numpy.linalg import norm
 from scipy.signal import butter, sosfiltfilt
 
+from skimu.utility import rolling_mean
 
-def metric_en(accel, *args, **kwargs):
+
+__all__ = ["metric_en", "metric_enmo", "metric_bfen", "metric_hfen", "metric_hfenplus"]
+
+
+def metric_en(accel, wlen, *args, **kwargs):
     """
     Euclidean norm
 
@@ -17,16 +22,18 @@ def metric_en(accel, *args, **kwargs):
     ----------
     accel : numpy.ndarray
         (N, 3) array of acceleration values in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
 
     Returns
     -------
     en : numpy.ndarray
         (N, ) array of euclidean norms.
     """
-    return norm(accel, axis=1)
+    return rolling_mean(norm(accel, axis=1), wlen, wlen)
 
 
-def metric_enmo(accel, *args, take_abs=False, trim_zero=True, **kwargs):
+def metric_enmo(accel, wlen, *args, take_abs=False, trim_zero=True, **kwargs):
     """
     Euclidean norm minus 1. Works best when the accelerometer data has been calibrated so that
     at rest the norm meaures 1g.
@@ -35,6 +42,8 @@ def metric_enmo(accel, *args, take_abs=False, trim_zero=True, **kwargs):
     ----------
     accel : numpy.ndarray
         (N, 3) array of acceleration values in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
     take_abs : bool, optional
         Use the absolute value of the difference between euclidean norm and 1g. Default is False.
     trim_zero : bool, optional
@@ -49,12 +58,12 @@ def metric_enmo(accel, *args, take_abs=False, trim_zero=True, **kwargs):
     if take_abs:
         enmo = abs(enmo)
     if trim_zero:
-        return minimum(enmo, 0)
+        return rolling_mean(minimum(enmo, 0), wlen, wlen)
     else:
-        return norm(accel, axis=1) - 1
+        return rolling_mean(enmo, wlen, wlen)
 
 
-def metric_bfen(accel, fs, low_cutoff=0.2, high_cutoff=15, trim_zero=True, **kwargs):
+def metric_bfen(accel, wlen, fs, low_cutoff=0.2, high_cutoff=15, trim_zero=True, **kwargs):
     """
     Band-pass filtered euclidean norm.
 
@@ -62,6 +71,8 @@ def metric_bfen(accel, fs, low_cutoff=0.2, high_cutoff=15, trim_zero=True, **kwa
     ----------
     accel : numpy.ndarray
         (N, 3) array of acceleration values in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
     fs : float
         Sampling frequency of `accel` in Hz.
     low_cutoff : float, optional
@@ -78,12 +89,12 @@ def metric_bfen(accel, fs, low_cutoff=0.2, high_cutoff=15, trim_zero=True, **kwa
     """
     sos = butter(4, [2 * low_cutoff / fs, 2 * high_cutoff / fs], btype='bandpass', output='sos')
     if trim_zero:
-        return minimum(norm(sosfiltfilt(sos, accel, axis=0), axis=1), 0)
+        return rolling_mean(minimum(norm(sosfiltfilt(sos, accel, axis=0), axis=1), 0), wlen, wlen)
     else:
-        return norm(sosfiltfilt(sos, accel, axis=0), axis=1)
+        return rolling_mean(norm(sosfiltfilt(sos, accel, axis=0), axis=1), wlen, wlen)
 
 
-def metric_hfen(accel, fs, low_cutoff=0.2, trim_zero=True, **kwargs):
+def metric_hfen(accel, wlen, fs, low_cutoff=0.2, trim_zero=True, **kwargs):
     """
     High-pass filtered euclidean norm.
 
@@ -91,6 +102,8 @@ def metric_hfen(accel, fs, low_cutoff=0.2, trim_zero=True, **kwargs):
     ----------
     accel : numpy.ndarray
         (N, 3) array of acceleration values in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
     fs : float
         Sampling frequency of `accel` in Hz.
     low_cutoff : float, optional
@@ -106,12 +119,12 @@ def metric_hfen(accel, fs, low_cutoff=0.2, trim_zero=True, **kwargs):
     sos = butter(4, 2 * low_cutoff / fs, btype='high', output='sos')
     
     if trim_zero:
-        return minimum(norm(sosfiltfilt(sos, accel, axis=0), axis=1), 0)
+        return rolling_mean(minimum(norm(sosfiltfilt(sos, accel, axis=0), axis=1), 0), wlen, wlen)
     else:
-        return norm(sosfiltfilt(sos, accel, axis=0), axis=1)
+        return rolling_mean(norm(sosfiltfilt(sos, accel, axis=0), axis=1), wlen, wlen)
 
 
-def metric_hfenplus(accel, fs, cutoff=0.2, trim_zero=True, **kwargs):
+def metric_hfenplus(accel, wlen, fs, cutoff=0.2, trim_zero=True, **kwargs):
     """
     High-pass filtered euclidean norm plus the low-pass filtered euclidean norm minus 1g.
 
@@ -119,6 +132,8 @@ def metric_hfenplus(accel, fs, cutoff=0.2, trim_zero=True, **kwargs):
     ----------
     accel : numpy.ndarray
         (N, 3) array of acceleration values in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
     fs : float
         Sampling frequency of `accel` in Hz.
     cutoff : float, optional
@@ -139,6 +154,29 @@ def metric_hfenplus(accel, fs, cutoff=0.2, trim_zero=True, **kwargs):
     acc_low = norm(sosfiltfilt(sos_low, accel, axis=0), axis=1)
     
     if trim_zero:
-        return minimum(acc_high + acc_low - 1)
+        return rolling_mean(minimum(acc_high + acc_low - 1, 0), wlen, wlen)
     else:
-        return acc_high + acc_low - 1
+        return rolling_mean(acc_high + acc_low - 1, wlen, wlen)
+
+
+def metric_mad(accel, wlen, *args, **kwargs):
+    """
+    Compute the Mean Amplitude Deviation metric for acceleration.
+
+    Parameters
+    ----------
+    accel : numpy.ndarray
+        (N, 3) array of accelerationes measured in g.
+    wlen : int
+        Window length (in number of samples) for non-overlapping windows.
+
+    Returns
+    -------
+    mad : numpy.ndarray
+        (N, ) array of computed MAD values.
+    """
+    acc_norm = norm(accel, axis=1)
+    r_avg = repeat(rolling_mean(acc_norm, wlen, wlen), wlen)
+
+    mad = rolling_mean(abs(acc_norm[:r_avg.size] - r_avg), wlen, wlen)
+    return mad
