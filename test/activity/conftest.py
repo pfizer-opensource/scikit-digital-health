7ed057f93b904c7c3c651431c3f07d4204239822
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-03 15:07:39 -0500

    Add another test for MVPA bouts that are at the start and end of the sample data

--- test/activity/conftest.py
+++ test/activity/conftest.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 @fixture(scope="module")
-def get_sample_activity_bout_data():
+def get_sample_activity_bout_data1():
     def get_data(boutmetric, boutdur):
         rr1 = np.zeros(500)
         rr1[49:90] = 1
@@ -36,6 +36,50 @@ def get_sample_activity_bout_data():
                 ret += (22.75,)
             elif boutmetric == 5:
                 ret += (22.75,)
+
+        return ret
+    return get_data
+
+
+@fixture(scope="module")
+def get_sample_activity_bout_data2():
+    def get_data(boutmetric, boutdur):
+        rr1 = np.zeros(500)
+        rr1[0:75] = 1
+        rr1[84:90] = 1
+        rr1[94:100] = 1
+        rr1[119:220] = 1
+        rr1[399:417] = 1
+        rr1[420:422] = 1
+        rr1[423:425] = 1
+        rr1[426:429] = 1
+        rr1[432:434] = 1
+        rr1[435:437] = 1
+        rr1[449:500] = 1
+
+        ret = (rr1,)
+        if boutdur == 1:
+            if boutmetric == 1:
+                ret += (22.0,)
+            elif boutmetric == 2:
+                ret += (21.333333333333,)
+            elif boutmetric == 3:
+                ret += (20.5,)
+            elif boutmetric == 4:
+                ret += (19.5,)
+            elif boutmetric == 5:
+                ret += (19.4166666666666,)
+        elif boutdur == 2:
+            if boutmetric == 1:
+                ret += (22.0,)
+            elif boutmetric == 2:
+                ret += (22.25,)
+            elif boutmetric == 3:
+                ret += (21.583333333333,)
+            elif boutmetric == 4:
+                ret += (20.0,)
+            elif boutmetric == 5:
+                ret += (19.916666666666,)
 
         return ret
     return get_data
--- test/activity/test_activity.py
+++ test/activity/test_activity.py
@@ -12,32 +12,36 @@ from skimu.activity.core import get_activity_bouts
 
 
 class TestGetActivityBouts:
-    @pytest.mark.parametrize(
-        ("boutdur", "boutmetric"),
-        (
-                (1, 1),
-                (1, 2),
-                (1, 3),
-                (1, 4),
-                (1, 5),
-                (2, 1),
-                (2, 2),
-                (2, 3),
-                (2, 4),
-                (2, 5)
-        )
-    )
-    def test(self, boutdur, boutmetric, get_sample_activity_bout_data):
-        sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data(boutmetric, boutdur)
+    @pytest.mark.parametrize("boutdur", (1, 2))
+    def test_no_mvpa_at_ends(self, boutdur, get_sample_activity_bout_data1):
+        for boutmetric in range(1, 6):
+            sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data1(boutmetric, boutdur)
 
-        mvpa_time = get_activity_bouts(
-            sample_acc_metric,
-            mvpa_thresh=0.1,
-            wlen=5,
-            boutdur=boutdur,
-            boutcrit=0.8,
-            closedbout=False,
-            boutmetric=boutmetric
-        )
+            mvpa_time = get_activity_bouts(
+                sample_acc_metric,
+                mvpa_thresh=0.1,
+                wlen=5,
+                boutdur=boutdur,
+                boutcrit=0.8,
+                closedbout=False,
+                boutmetric=boutmetric
+            )
 
-        assert isclose(mvpa_time, mvpa_time_true)
+            assert isclose(mvpa_time, mvpa_time_true)
+
+    @pytest.mark.parametrize("boutdur", (1, 2))
+    def test_mvpa_at_ends(self, boutdur, get_sample_activity_bout_data2):
+        for boutmetric in range(1, 6):
+            sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data2(boutmetric, boutdur)
+
+            mvpa_time = get_activity_bouts(
+                sample_acc_metric,
+                mvpa_thresh=0.1,
+                wlen=5,
+                boutdur=boutdur,
+                boutcrit=0.8,
+                closedbout=False,
+                boutmetric=boutmetric
+            )
+
+            assert isclose(mvpa_time, mvpa_time_true)
