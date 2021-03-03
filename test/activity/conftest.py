e324fac7caf5570426f4bdc80389e928b6052e22
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-03 13:48:56 -0500

    Add tests for bout duration of 2

--- test/activity/conftest.py
+++ test/activity/conftest.py
@@ -4,7 +4,7 @@ import numpy as np
 
 @fixture(scope="module")
 def get_sample_activity_bout_data():
-    def get_data(boutmetric):
+    def get_data(boutmetric, boutdur):
         rr1 = np.zeros(500)
         rr1[49:90] = 1
         rr1[99:115] = 1
@@ -14,16 +14,28 @@ def get_sample_activity_bout_data():
         rr1[372:450] = 1
 
         ret = (rr1,)
-        if boutmetric == 1:
-            ret += (23.75,)
-        elif boutmetric == 2:
-            ret += (24.333333333333,)
-        elif boutmetric == 3:
-            ret += (24.75,)
-        elif boutmetric == 4:
-            ret += (23.0833333333333,)
-        elif boutmetric == 5:
-            ret += (23.0833333333333,)
+        if boutdur == 1:
+            if boutmetric == 1:
+                ret += (23.75,)
+            elif boutmetric == 2:
+                ret += (24.333333333333,)
+            elif boutmetric == 3:
+                ret += (24.75,)
+            elif boutmetric == 4:
+                ret += (23.0833333333333,)
+            elif boutmetric == 5:
+                ret += (23.0833333333333,)
+        elif boutdur == 2:
+            if boutmetric == 1:
+                ret += (23.75,)
+            elif boutmetric == 2:
+                ret += (24.4166666666666,)
+            elif boutmetric == 3:
+                ret += (25.0833333333333,)
+            elif boutmetric == 4:
+                ret += (22.75,)
+            elif boutmetric == 5:
+                ret += (22.75,)
 
         return ret
     return get_data
--- test/activity/test_activity.py
+++ test/activity/test_activity.py
@@ -12,15 +12,29 @@ from skimu.activity.core import get_activity_bouts
 
 
 class TestGetActivityBouts:
-    @pytest.mark.parametrize("boutmetric", (1, 2, 3, 4, 5))
-    def test(self, boutmetric, get_sample_activity_bout_data):
-        sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data(boutmetric)
+    @pytest.mark.parametrize(
+        ("boutdur", "boutmetric"),
+        (
+                (1, 1),
+                (1, 2),
+                (1, 3),
+                (1, 4),
+                (1, 5),
+                (2, 1),
+                (2, 2),
+                (2, 3),
+                (2, 4),
+                (2, 5)
+        )
+    )
+    def test(self, boutdur, boutmetric, get_sample_activity_bout_data):
+        sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data(boutmetric, boutdur)
 
         mvpa_time = get_activity_bouts(
             sample_acc_metric,
             mvpa_thresh=0.1,
             wlen=5,
-            boutdur=1,
+            boutdur=boutdur,
             boutcrit=0.8,
             closedbout=False,
             boutmetric=boutmetric
