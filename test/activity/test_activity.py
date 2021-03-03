bd62d290d14feb8c0afdbde5a517813a4a1fa829
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-03 16:06:11 -0500

    Move tests back to generating 1 for each boutmetric case

--- test/activity/test_activity.py
+++ test/activity/test_activity.py
@@ -12,36 +12,38 @@ from skimu.activity.core import get_activity_bouts
 
 
 class TestGetActivityBouts:
-    @pytest.mark.parametrize("boutdur", (1, 2))
-    def test_no_mvpa_at_ends(self, boutdur, get_sample_activity_bout_data1):
-        for boutmetric in range(1, 6):
-            sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data1(boutmetric, boutdur)
+    @pytest.mark.parametrize(
+        "boutdur,boutmetric", [[i, j] for i in range(1, 3) for j in range(1, 6)]
+    )
+    def test_no_mvpa_at_ends(self, boutdur, boutmetric, get_sample_activity_bout_data1):
+        sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data1(boutmetric, boutdur)
 
-            mvpa_time = get_activity_bouts(
-                sample_acc_metric,
-                mvpa_thresh=0.1,
-                wlen=5,
-                boutdur=boutdur,
-                boutcrit=0.8,
-                closedbout=False,
-                boutmetric=boutmetric
-            )
+        mvpa_time = get_activity_bouts(
+            sample_acc_metric,
+            mvpa_thresh=0.1,
+            wlen=5,
+            boutdur=boutdur,
+            boutcrit=0.8,
+            closedbout=False,
+            boutmetric=boutmetric
+        )
 
-            assert isclose(mvpa_time, mvpa_time_true)
+        assert isclose(mvpa_time, mvpa_time_true)
 
-    @pytest.mark.parametrize("boutdur", (1, 2))
-    def test_mvpa_at_ends(self, boutdur, get_sample_activity_bout_data2):
-        for boutmetric in range(1, 6):
-            sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data2(boutmetric, boutdur)
+    @pytest.mark.parametrize(
+        "boutdur,boutmetric", [[i, j] for i in range(1, 3) for j in range(1, 6)]
+    )
+    def test_mvpa_at_ends(self, boutdur, boutmetric, get_sample_activity_bout_data2):
+        sample_acc_metric, mvpa_time_true = get_sample_activity_bout_data2(boutmetric, boutdur)
 
-            mvpa_time = get_activity_bouts(
-                sample_acc_metric,
-                mvpa_thresh=0.1,
-                wlen=5,
-                boutdur=boutdur,
-                boutcrit=0.8,
-                closedbout=False,
-                boutmetric=boutmetric
-            )
+        mvpa_time = get_activity_bouts(
+            sample_acc_metric,
+            mvpa_thresh=0.1,
+            wlen=5,
+            boutdur=boutdur,
+            boutcrit=0.8,
+            closedbout=False,
+            boutmetric=boutmetric
+        )
 
-            assert isclose(mvpa_time, mvpa_time_true)
+        assert isclose(mvpa_time, mvpa_time_true)
