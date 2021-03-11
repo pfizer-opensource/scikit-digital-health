765162e34ba07278c24550c175853002d0c2d887
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-11 09:25:02 -0500

    Add rolling_median

--- src/skimu/utility/math.py
+++ src/skimu/utility/math.py
@@ -4,12 +4,12 @@ Utility math functions
 Lukas Adamowicz
 Pfizer DMTI 2021
 """
-from numpy import moveaxis
+from numpy import moveaxis, nan
 
 from skimu.utility import _extensions
 
 
-__all__ = ["rolling_mean", "rolling_sd", "rolling_skewness", "rolling_kurtosis"]
+__all__ = ["rolling_mean", "rolling_sd", "rolling_skewness", "rolling_kurtosis", "rolling_median"]
 
 
 def rolling_mean(a, w_len, skip, axis=-1):
@@ -418,3 +418,99 @@ def rolling_kurtosis(a, w_len, skip, axis=-1, return_previous=True):
         return tuple(moveaxis(i, -1, axis) for i in res)
     else:
         return moveaxis(res, -1, axis)
+
+
+def rolling_median(a, w_len, skip=1, pad=False, axis=-1):
+    r"""
+    Compute the rolling mean.
+
+    Parameters
+    ----------
+    a : array-like
+        Signal to compute rolling mean for.
+    w_len : int
+        Window length in number of samples.
+    skip : int
+        Window start location skip in number of samples. Default is 1.
+    pad : {bool, int, float}, optional
+        Pad the returned array length with either `nan` (`True`), or the pad value. False does not
+        pad the array with values. Note that the windows are left-justified, so the padded values
+        will be on the end of the array (`rmed[w_len-1:]` for `skip=1`).
+    axis : int, optional
+        Axis to compute the rolling mean along. Default is -1.
+
+    Returns
+    -------
+    rmed : numpy.ndarray
+        Rolling median. Note that if the rolling axis is not the last axis, then the result
+        will *not* be c-contiguous.
+
+    Notes
+    -----
+    On the rolling axis, the output length can be computed as follows:
+
+    .. math:: \frac{n - w_{len}}{skip} + 1
+
+    where `n` is the length of the rolling axis. Computation efficiency does not decrease for
+    increasing values of `skip` due to the underlying algorithms.
+
+    Examples
+    --------
+    Compute the with non-overlapping windows:
+
+    >>> import numpy as np
+    >>> x = np.arange(10)
+    >>> rolling_median(x, 3, 3)
+    array([1., 4., 7.])
+
+    Compute with overlapping windows:
+
+    >>> rolling_mean(x, 3, 1)
+    array([1., 2., 3., 4., 5., 6., 7., 8.])
+
+    Compute on a nd-array to see output shape. On the rolling axis, the output should be equal to
+    :math:`(n - w_{len}) / skip + 1`.
+
+    >>> n = 500
+    >>> window_length = 100
+    >>> window_skip = 50
+    >>> shape = (3, n, 5, 10)
+    >>> y = np.random.random(shape)
+    >>> res = rolling_mean(y, window_length, window_skip, axis=1)
+    >>> print(res.shape)
+    (3, 9, 5, 10)
+
+    Check flags for different axis output
+
+    >>> z = np.random.random((10, 10, 10))
+    >>> rolling_mean(z, 3, 3, axis=0).flags['C_CONTIGUOUS']
+    False
+
+    >>> rolling_mean(z, 3, 3, axis=1).flags['C_CONTIGUOUS']
+    False
+
+    >>> rolling_mean(z, 3, 3, axis=2).flags['C_CONTIGUOUS']
+    True
+    """
+    if w_len <= 0 or skip <= 0:
+        raise ValueError("`wlen` and `skip` cannot be less than or equal to 0.")
+
+    # move computation axis to end
+    x = moveaxis(a, axis, -1)
+
+    # check that there are enough samples
+    if w_len > x.shape[-1]:
+        raise ValueError("Cannot have a window length larger than the computation axis.")
+
+    rmed = _extensions.rolling_median(x, w_len)
+
+    if isinstance(pad, bool):
+        if pad:
+            rmed[..., -(w_len - 1):] = nan
+        else:
+            rmed = rmed[..., :-w_len + 1]
+    else:
+        rmed[..., -(w_len - 1):] = pad
+
+    # move computation axis back to original place and return
+    return moveaxis(rmed[..., ::skip], -1, axis)
