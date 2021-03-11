5d76ccc6f90538823c180ff299ceb0d35aabee50
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-11 11:17:20 -0500

    Expand tests to make testing clearer

--- test/sleep/test_utilities.py
+++ test/sleep/test_utilities.py
@@ -12,21 +12,27 @@ from skimu.sleep.utility import *
 class TestRLE:
     def test_full_expected_input(self):
         arr = [0] * 5 + [1] * 3 + [0] * 4 + [1] * 7 + [0] * 2 + [1] * 6 + [0] * 1
-        lengths = np.asarray([5, 3, 4, 7, 2, 6, 1])
-        indices = np.asarray([0, 5, 8, 12, 19, 21, 27])
-        values = np.asarray([0, 1, 0, 1, 0, 1, 0])
-        expected = (lengths, indices, values)
-        out = rle(arr)
-        assert np.array_equal(expected, out)
+        exp_lengths = np.asarray([5, 3, 4, 7, 2, 6, 1])
+        exp_indices = np.asarray([0, 5, 8, 12, 19, 21, 27])
+        exp_values = np.asarray([0, 1, 0, 1, 0, 1, 0])
+
+        lengths, starts, vals = rle(arr)
+
+        assert np.allclose(lengths, exp_lengths)
+        assert np.allclose(starts, exp_indices)
+        assert np.allclose(vals, exp_values)
 
     def test_single_val(self):
         arr = [0] * 50
-        lengths = np.asarray([50])
-        indices = np.asarray([0])
-        values = np.asarray([0])
-        expected = (lengths, indices, values)
-        out = rle(arr)
-        assert np.array_equal(expected, out)
+        exp_lengths = np.asarray([50])
+        exp_indices = np.asarray([0])
+        exp_values = np.asarray([0])
+
+        lengths, starts, vals = rle(arr)
+
+        assert np.allclose(lengths, exp_lengths)
+        assert np.allclose(starts, exp_indices)
+        assert np.allclose(vals, exp_values)
 
 
 class TestComputeZAngle:
