eac7b9bcc50d5ae75a071e920b86c354ba4344ed
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-11 10:32:43 -0500

    Remove old tso detection test

--- test/sleep/test_tso.py
+++ test/sleep/test_tso.py
@@ -17,21 +17,3 @@ class TestDetectTSO:
 
         assert abs(tso[0] - sleep[0]) < 30  # less than 30 seconds off
         assert abs(tso[1] - sleep[1]) < 30
-
-"""
-def check_tso_detection():
-    from skimu.read import ReadBin
-
-    src = "/Users/ladmin/Desktop/PfyMU_development/sleeppy_pfymu/test_data/demo.bin"
-    reader = ReadBin(base=12, period=24)
-    res = reader.predict(src)
-    for day in res["day_ends"]:
-        acc = res["accel"][day[0]: day[1]]
-        t = res["time"][day[0]: day[1]]
-        fs = 100
-        temp = res["temperature"][day[0] : day[1]]
-        temp = repeat(temp, 300)
-        out = detect_tso(acc=acc, t=t, fs=fs, temp=temp)
-    print(out)
-    print(to_datetime(out[0], unit="s"), to_datetime(out[1], unit="s"))
-"""
