c33fca791a80f7be0fd4ccdc51dd4e02812c582f
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-17 10:53:25 -0400

    write activity index

---
+++ src/skimu/sleep/activity_index.py
@@ -0,0 +1,24 @@
+"""
+Compute the activity index from acceleration data
+
+Yiorgos Christakis, Lukas Adamowicz
+Pfizer DMTI 2019-2021
+"""
+from scipy.signal import butter, sosfiltfilt
+from numpy import sqrt, mean, var
+
+from skimu.utility import get_windowed_view
+
+
+def calculate_activity_index(fs, accel, hp_cut=0.25):
+    # high pass filter
+    sos = butter(3, hp_cut * 2 / fs, btype="high", output="sos")
+    accel_hf = sosfiltfilt(sos, accel, axis=0)
+
+    # non-overlapping 60s windows
+    acc_w = get_windowed_view(accel_hf, int(60 * fs), int(60 * fs))
+
+    # compute activity index
+    act_ind = sqrt(mean(var(acc_w, axis=2), axis=1))
+
+    return act_ind
