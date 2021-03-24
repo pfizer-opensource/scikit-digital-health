5826700ab0f98a8389e207f5b0bde55424c55b15
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-24 10:56:41 -0400

    Create base endpoint testing class Create sleep average hazard tests

--- test/sleep/test_endpoints.py
+++ test/sleep/test_endpoints.py
@@ -1,17 +1,51 @@
 import numpy as np
 
 from skimu.sleep.utility import rle
-from skimu.sleep.endpoints import SleepAverageHazard
+from skimu.sleep.endpoints import *
 
 
-class TestSleepAverageHazard:
-    def test(self):
-        x = np.array(
-            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-             0]
+class _BaseTestEndpoint:
+    def test(self, dummy_sleep_predictions):
+        l, s, v = rle(dummy_sleep_predictions)
+
+        res = self.metric().predict(
+            sleep_predictions=dummy_sleep_predictions,
+            lengths=l,
+            starts=s,
+            values=v
         )
-        l, s, v = rle(x)
 
-        sh = SleepAverageHazard().predict(lengths=l, starts=s, values=v)
+        assert np.isclose(res, self.normal_result, equal_nan=True)
 
-        assert np.isclose(sh, 0.8333333)
+    def test_all_zeros(self):
+        sleep_pred = np.zeros(40)
+        l, s, v = rle(sleep_pred)
+
+        res = self.metric().predict(
+            sleep_predictions=sleep_pred,
+            lengths=l,
+            starts=s,
+            values=v
+        )
+
+        assert np.isclose(res, self.zeros_result, equal_nan=True)
+
+    def test_all_ones(self):
+        sleep_pred = np.ones(40)
+        l, s, v = rle(sleep_pred)
+
+        res = self.metric().predict(
+            sleep_predictions=sleep_pred,
+            lengths=l,
+            starts=s,
+            values=v
+        )
+
+        assert np.isclose(res, self.ones_result, equal_nan=True)
+
+
+class TestSleepAverageHazard(_BaseTestEndpoint):
+    metric = SleepAverageHazard
+    normal_result = 0.8333333
+    zeros_result = np.nan
+    ones_result = 1.0
