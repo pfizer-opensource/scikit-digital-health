60761e586eb3eaeebdc6d3d3e003d5dc61f03ed5
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-24 10:40:23 -0400

    Fleshing out init and module documentation

--- src/skimu/sleep/__init__.py
+++ src/skimu/sleep/__init__.py
@@ -1,1 +1,54 @@
+"""
+IMU Sleep Analysis (:mod:`skimu.sleep`)
+=======================================
+
+.. currentmodule:: skimu.sleep
+
+Pipeline sleep processing
+-------------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    Sleep
+
+.. _sleep-metrics:
+
+Sleep Metrics
+-------------
+
+.. autosummary::
+    :toctree: generated/
+
+    TotalSleepTime
+    PercentTimeAsleep
+    NumberWakeBouts
+    SleepOnsetLatency
+    WakeAfterSleepOnset
+    AverageSleepDuration
+    AverageWakeDuration
+    SleepWakeTransitionProbability
+    WakeSleepTransitionProbability
+    SleepGiniIndex
+    WakeGiniIndex
+    SleepAverageHazard
+    WakeAverageHazard
+    SleepPowerLawDistribution
+    WakePowerLawDistribution
+
+
+Background Information
+----------------------
+
+
+Adding Custom Sleep Metrics
+---------------------------
+
+"""
+
 from skimu.sleep.sleep import Sleep
+from skimu.sleep import sleep
+from skimu.sleep.endpoints import *
+from skimu.sleep import endpoints
+
+__all__ = ["Sleep", "endpoints"] + endpoints.__all__
