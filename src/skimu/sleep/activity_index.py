bf0c44d7ba52a9b42286bd620bf8aab8904d7b3f
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-24 08:37:03 -0400

    Broadcast filter output to c-contiguous array so it can be windowed

--- src/skimu/sleep/activity_index.py
+++ src/skimu/sleep/activity_index.py
@@ -5,7 +5,7 @@ Yiorgos Christakis, Lukas Adamowicz
 Pfizer DMTI 2019-2021
 """
 from scipy.signal import butter, sosfiltfilt
-from numpy import sqrt, mean, var
+from numpy import sqrt, mean, var, ascontiguousarray
 
 from skimu.utility import get_windowed_view
 
@@ -13,7 +13,7 @@ from skimu.utility import get_windowed_view
 def calculate_activity_index(fs, accel, hp_cut=0.25):
     # high pass filter
     sos = butter(3, hp_cut * 2 / fs, btype="high", output="sos")
-    accel_hf = sosfiltfilt(sos, accel, axis=0)
+    accel_hf = ascontiguousarray(sosfiltfilt(sos, accel, axis=0))
 
     # non-overlapping 60s windows
     acc_w = get_windowed_view(accel_hf, int(60 * fs), int(60 * fs))
