1fe24b40ab673cab2c0c61a29f6db33339492261
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-03-26 09:48:11 -0400

    Skimu now imports sleep

--- src/skimu/__init__.py
+++ src/skimu/__init__.py
@@ -19,18 +19,20 @@ from skimu.pipeline import Pipeline
 from skimu import utility
 from skimu import read
 from skimu import preprocessing
+from skimu import sleep
 from skimu import gait
 from skimu import sit2stand
 from skimu import features
 
 __skimu_version__ = __version__
 __all__ = [
-    'Pipeline',
-    'gait',
-    'sit2stand',
-    'read',
-    'preprocessing',
-    'features',
-    'utility',
-    '__skimu_version__'
+    "Pipeline",
+    "gait",
+    "sit2stand",
+    "read",
+    "sleep",
+    "preprocessing",
+    "features",
+    "utility",
+    "__skimu_version__"
 ]
