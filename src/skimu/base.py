9e45675fa490a7b442712b2973c0258b96178396
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-04-07 09:13:39 -0400

    Add a file name attribute

--- src/skimu/base.py
+++ src/skimu/base.py
@@ -51,7 +51,7 @@ class _BaseProcess:
         self.logger = logging.getLogger(__name__)
 
         # for plotting
-        self.f = self.ax = None
+        self.f = self.ax = self.fname = None
 
     def predict(self, *args, **kwargs):
         """
