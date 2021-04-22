e6bae23884d187f8096d9e98fd47bcdc776d9a2d
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-04-22 11:18:26 -0400

    preprocessing now imports internal wear as _internal_wear

--- src/skimu/preprocessing/__init__.py
+++ src/skimu/preprocessing/__init__.py
@@ -17,4 +17,6 @@ from skimu.preprocessing import calibrate
 from skimu.preprocessing.wear_detection import DetectWear
 from skimu.preprocessing import wear_detection
 
+from skimu.preprocessing import internal_wear as _internal_wear
+
 __all__ = ("CalibrateAccelerometer", "calibrate", "DetectWear", "wear_detection")
