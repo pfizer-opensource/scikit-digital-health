8a999f39397151f8e991da66ce93c8e34ec1ed2f
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-04-23 10:49:33 -0400

    Remove internal_wear as it is now covered inside sleep itself

--- src/skimu/preprocessing/__init__.py
+++ src/skimu/preprocessing/__init__.py
@@ -17,6 +17,4 @@ from skimu.preprocessing import calibrate
 from skimu.preprocessing.wear_detection import DetectWear
 from skimu.preprocessing import wear_detection
 
-from skimu.preprocessing import internal_wear as _internal_wear
-
 __all__ = ("CalibrateAccelerometer", "calibrate", "DetectWear", "wear_detection")
