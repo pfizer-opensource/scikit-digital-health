25a27f7b94654100bee61f775308bb0865356aad
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-04-29 10:29:16 -0400

    Update read extensions for axivity and geneactiv into one combined library/extension for re-using some code Update axivity to handle multiple different bases and periods for windowing

---
+++ src/skimu/read/_extensions/pyread.c
@@ -0,0 +1,460 @@
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "numpy/arrayobject.h"
+
+#include "read_binary_imu.h"
+
+#define STR2PY PyUnicode_FromString
+
+#define NP_FROM_ANY(x) PyArray_FromAny(x, PyArray_DescrFromType(NPY_LONG), 1, 0, NPY_ARRAY_ENSUREARRAY | NPY_ARRAY_CARRAY_RO, NULL)
+
+
+void geneactiv_set_error_message(int ierr)
+{
+    switch (ierr)
+    {
+        case GN_READ_E_BLOCK_TIMESTAMP :
+            PyErr_SetString(PyExc_RuntimeError, "Error reading timestamp from data block");
+            break;
+        case GN_READ_E_BLOCK_FS :
+            PyErr_SetString(PyExc_RuntimeError, "Block sampling frequency does not match header");
+            break;
+        case GN_READ_E_BLOCK_DATA :
+            PyErr_SetString(PyExc_RuntimeError, "Error reading data from data block");
+            break;
+        case GN_READ_E_BLOCK_DATA_3600 :
+            PyErr_SetString(PyExc_RuntimeError, "Data length is shorter than 3600");
+            break;
+        default :
+            PyErr_SetString(PyExc_RuntimeError, "Unkown error reading GeneActiv file");
+    }
+}
+
+void axivity_set_error_message(int ierr)
+{
+    switch(ierr)
+    {
+        case AX_READ_E_BAD_HEADER :
+            PyErr_SetString(PyExc_RuntimeError, "Bad packet header value.");
+            break;
+        case AX_READ_E_MISMATCH_N_AXES :
+            PyErr_SetString(PyExc_RuntimeError, "Incorrect number of axes given file configuration.");
+            break;
+        case AX_READ_E_INVALID_BLOCK_SAMPLES :
+            PyErr_SetString(PyExc_RuntimeError, "Invalid number of samples in data block.");
+            break;
+        case AX_READ_E_BAD_AXES_PACKED :
+            PyErr_SetString(PyExc_RuntimeError, "More than 3 axes for packed data.");
+            break;
+        case AX_READ_E_BAD_PACKING_CODE :
+            PyErr_SetString(PyExc_RuntimeError, "Invalid packing code (not 0 or 2).");
+            break;
+        case AX_READ_E_BAD_CHECKSUM :
+            PyErr_SetString(PyExc_RuntimeError, "Checksum not equal to 0.");
+            break;
+        default :
+            PyErr_SetString(PyExc_RuntimeError, "Unkown error reading Axivity file");
+    }
+}
+
+static PyObject *read_axivity(PyObject *NPY_UNUSED(self), PyObject *args)
+{
+    PyObject* bytes;
+    char *file;
+    Py_ssize_t flen;
+    int ierr = AX_READ_E_NONE, fail = 0;
+    PyObject *bases_, *periods_;
+
+    AX_Info_t info;
+    Window_t winfo;
+
+    /* READ INPUT ARGUMENTS */
+    if (!PyArg_ParseTuple(args, "sOO:read_axivity", &file, &bases_, &periods_))
+        return NULL;
+    flen = strlen(file);
+    
+    /* GET NUMPY ARRAYS */
+    PyArrayObject *bases = (PyArrayObject *)NP_FROM_ANY(bases_);
+    PyArrayObject *periods = (PyArrayObject *)NP_FROM_ANY(periods_);
+
+    if (!bases || !periods)
+    {
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        return NULL;
+    }
+
+    /* WINDOWING INFO INIT */
+    winfo.n = PyArray_Size(bases);
+    if (winfo.n != PyArray_Size(periods))
+    {
+        Py_XDECREF(bases); Py_XDECREF(periods);
+        PyErr_SetString(PyExc_ValueError, "Size mismatch between bases and periods.");
+        return NULL;
+    }
+    winfo.i_start = (long *)malloc(winfo.n * sizeof(winfo.i_start));
+    winfo.i_stop = (long *)malloc(winfo.n * sizeof(winfo.i_stop));
+    winfo.bases = (long *)PyArray_DATA(bases);
+    winfo.periods = (long *)PyArray_DATA(periods);
+    /* set the indices to 0 initially */
+    memset(winfo.i_start, 0, winfo.n * sizeof(winfo.i_start));
+    memset(winfo.i_stop, 0, winfo.n * sizeof(winfo.i_stop));
+
+    /* INITIALIZATION */
+    info.nblocks = -1;
+    info.axes = -1;
+    info.count = -1;
+    info.max_days = MAX_DAYS;
+    info.Nwin = winfo.n;
+
+    /* read the header */
+    axivity_read_header(&flen, file, &info, &ierr);
+
+    if ((info.nblocks == -1) || (info.axes == -1) || (info.count == -1))
+    {
+        axivity_close(&info);
+
+        free(winfo.i_start);
+        free(winfo.i_stop);
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        PyErr_SetString(PyExc_IOError, "Bad read on number of blocks, axes, or samples");
+        return NULL;
+    }
+
+    /* DIMENSIONS FOR RETURN VALUES */
+    npy_intp dim3[2] = {(info.nblocks - 2) * info.count, info.axes};
+    npy_intp dim1[1] = {(info.nblocks - 2) * info.count};
+    npy_intp dim_idx[2] = {MAX_DAYS, winfo.n};
+
+    /* DATA ARRAYS */
+    PyArrayObject *imudata = (PyArrayObject *)PyArray_ZEROS(2, dim3, NPY_DOUBLE, 0);
+    PyArrayObject *time  = (PyArrayObject *)PyArray_ZEROS(1, dim1, NPY_DOUBLE, 0);
+    PyArrayObject *light = (PyArrayObject *)PyArray_ZEROS(1, dim1, NPY_DOUBLE, 0);
+
+    PyArrayObject *starts = (PyArrayObject *)PyArray_ZEROS(2, dim_idx, NPY_LONG, 0);
+    PyArrayObject *stops  = (PyArrayObject *)PyArray_ZEROS(2, dim_idx, NPY_LONG, 0);
+
+    if (!imudata || !time || !light || !starts || !stops)
+    {   
+        axivity_close(&info);
+
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+
+        Py_XDECREF(imudata);
+        Py_XDECREF(time);
+        Py_XDECREF(light);
+        Py_XDECREF(starts);
+        Py_XDECREF(stops);
+
+        free(winfo.i_start);
+        free(winfo.i_stop);
+
+        return NULL;
+    }
+
+    /* SET POINTERS */
+    double *imu_p   = (double *)PyArray_DATA(imudata);
+    double *ts_p    = (double *)PyArray_DATA(time);
+    long *light_p = (long *)PyArray_DATA(light);
+    long *starts_p = (long *)PyArray_DATA(starts);
+    long *stops_p  = (long *)PyArray_DATA(stops);
+
+    /* READ FILE */
+    long pos = 0;
+    for (int i=2; i < info.nblocks; ++i)
+    {
+        pos = 512 * i + 1;  /* +1 to account for fortran numbering */
+        axivity_read_block(&info, &pos, imu_p, ts_p, light_p, winfo.bases, winfo.periods,
+            starts_p, winfo.i_start, stops_p, winfo.i_stop, &ierr);
+        
+        if (ierr != 0)
+        {
+            PyErr_SetString(PyExc_RuntimeError, "Error reading axivity data block.");
+            fail = 1;
+            break;
+        }
+    }
+
+    axivity_close(&info);
+    free(winfo.i_start);
+    free(winfo.i_stop);
+
+    /* decrease ref count if successful or failed */
+    Py_XDECREF(bases);
+    Py_XDECREF(periods);
+    
+    if (fail)
+    {
+        Py_XDECREF(imudata);
+        Py_XDECREF(time);
+        Py_XDECREF(light);
+        Py_XDECREF(starts);
+        Py_XDECREF(stops);
+
+        axivity_set_error_message(ierr);
+        return NULL;
+    }
+
+    return Py_BuildValue(
+        "fNNNNN",  /* need to use N to not increment reference counter */
+        info.frequency,
+        (PyObject *)imudata,
+        (PyObject *)time,
+        (PyObject *)light,
+        (PyObject *)starts,
+        (PyObject *)stops
+    );
+}
+
+
+static PyObject *read_geneactiv(PyObject *NPY_UNUSED(self), PyObject *args)
+{
+    char *file;
+    int ierr = GN_READ_E_NONE, fail = 0;
+    PyObject *bases_, *periods_;
+
+    FILE *fp;
+    GN_Info_t info;
+    GN_Data_t data;
+    Window_t winfo;
+
+    /* INITIALIZATION */
+    info.fs_err = 0;
+    info.max_n = 0;
+    info.npages = -1;
+
+    /* PYTHON ARGUMENTS */
+    if (!PyArg_ParseTuple(args, "sOO:read_geneactiv", &file, &bases_, &periods_))
+        return NULL;  /* error is set for us */
+    
+    /* GET NUMPY ARRAYS */
+    PyArrayObject *bases = (PyArrayObject *)NP_FROM_ANY(bases_);
+    PyArrayObject *periods = (PyArrayObject *)NP_FROM_ANY(periods_);
+
+    if (!bases || !periods)
+    {
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        return NULL;
+    }
+
+    /* WINDOING INFO INIT */
+    winfo.n = PyArray_Size(bases);
+    if (winfo.n != PyArray_Size(periods))
+    {
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        PyErr_SetString(PyExc_ValueError, "Size mismatch between bases and periods");
+        return NULL;
+    }
+    winfo.i_start = (long *)malloc(winfo.n * sizeof(winfo.i_start));
+    winfo.i_stop = (long *)malloc(winfo.n * sizeof(winfo.i_stop));
+    winfo.bases = (long *)PyArray_DATA(bases);
+    winfo.periods = (long *)PyArray_DATA(periods);
+    /* set the indices to 0 initially */
+    memset(winfo.i_start, 0, winfo.n * sizeof(winfo.i_stop));
+    memset(winfo.i_stop, 0, winfo.n * sizeof(winfo.i_stop));
+    
+    /* OPEN THE FILE */
+    fp = fopen(file, "r");
+    if (!fp)
+    {
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        PyErr_SetString(PyExc_IOError, "Error opening file");
+        return NULL;
+    }
+
+    /* READ THE HEADER */
+    geneactiv_read_header(fp, &info);
+
+    if (info.npages == -1)
+    {
+        fclose(fp);
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+        PyErr_SetString(PyExc_IOError, "Cannot read number of blocks");
+        return NULL;
+    }
+
+    /* DIMENSIONS FOR RETURN VALUES */
+    npy_intp dim3[2] = {info.npages * GN_SAMPLES, 3};
+    npy_intp dim1[1] = {info.npages * GN_SAMPLES};
+
+    npy_intp dim_idx[2] = {MAX_DAYS, winfo.n};
+
+    /* DATA ARRAYS */
+    PyArrayObject *accel = (PyArrayObject *)PyArray_ZEROS(2, dim3, NPY_DOUBLE, 0);
+    PyArrayObject *time  = (PyArrayObject *)PyArray_ZEROS(1, dim1, NPY_DOUBLE, 0);
+    PyArrayObject *light = (PyArrayObject *)PyArray_ZEROS(1, dim1, NPY_DOUBLE, 0);
+    PyArrayObject *temp  = (PyArrayObject *)PyArray_ZEROS(1, dim1, NPY_DOUBLE, 0);
+
+    PyArrayObject *starts = (PyArrayObject *)PyArray_ZEROS(2, dim_idx, NPY_LONG, 0);
+    PyArrayObject *stops  = (PyArrayObject *)PyArray_ZEROS(2, dim_idx, NPY_LONG, 0);
+
+    if (!accel || !time || !light || !temp || !starts || !stops)
+    {
+        fclose(fp);
+
+        Py_XDECREF(bases);
+        Py_XDECREF(periods);
+
+        Py_XDECREF(accel);
+        Py_XDECREF(time);
+        Py_XDECREF(temp);
+        Py_XDECREF(light);
+        Py_XDECREF(starts);
+        Py_XDECREF(stops);
+
+        free(winfo.i_start);
+        free(winfo.i_stop);
+
+        return NULL;
+    }
+
+    /* SET POINTERS */
+    data.acc   = (double *)PyArray_DATA(accel);
+    data.ts    = (double *)PyArray_DATA(time);
+    data.light = (double *)PyArray_DATA(light);
+    data.temp  = (double *)PyArray_DATA(temp);
+    data.day_starts = (long *)PyArray_DATA(starts);
+    data.day_stops  = (long *)PyArray_DATA(stops);
+    
+    /* READ FILE */
+    for (int i = 0; i < info.npages; ++i)
+    {
+        ierr = geneactiv_read_block(fp, &winfo, &info, &data);
+
+        if (ierr == GN_READ_E_BLOCK_FS_WARN)
+        {
+            int err_ret = PyErr_WarnEx(PyExc_RuntimeWarning, warn_str, 1);
+
+            if (err_ret == -1)  /* warnings are being raised as exceptions */
+            {
+                fail = 1;
+                break;
+            }
+        }
+        else if (ierr != GN_READ_E_NONE)
+        {
+            fail = 1;
+            break;
+        }
+    }
+
+    fclose(fp);
+    free(winfo.i_start);
+    free(winfo.i_stop);
+
+    /* decrease ref count if successful or failed */
+    Py_XDECREF(bases);
+    Py_XDECREF(periods);
+
+    if (fail)
+    {
+        Py_XDECREF(accel);
+        Py_XDECREF(time);
+        Py_XDECREF(temp);
+        Py_XDECREF(light);
+        Py_XDECREF(starts);
+        Py_XDECREF(stops);
+
+        geneactiv_set_error_message(ierr);
+        return NULL;
+    }
+
+    return Py_BuildValue(
+        "lfNNNNNN",  /* need to use N to not increment reference counter */
+        info.max_n,
+        info.fs,
+        (PyObject *)accel,
+        (PyObject *)time,
+        (PyObject *)light,
+        (PyObject *)temp,
+        (PyObject *)starts,
+        (PyObject *)stops
+    );
+}
+
+
+static const char read_axivity__doc__[] = "read_axivity(file, bases, periods)\n"
+"Read an Axivity binary file.\n\n"
+"Parameters\n"
+"----------\n"
+"file : str\n"
+"   File name to read from\n"
+"bases : numpy.ndarray\n"
+"   Base times for providing windowing. Must be in [0, 23].\n"
+"periods : numpy.ndarray\n"
+"   Number of hours for each window. Must be in [1, 24] and the same size as bases.\n\n"
+"Returns\n"
+"-------\n"
+"fs : float\n"
+"   Sampling frequency\n"
+"imudata : numpy.ndarray\n"
+"   IMU data available. Shape is (N, 3/6/9). Order of types is [Gy]Ax[Mag]. Ax (accelerometer) is\n"
+"   required. Gy (gyroscope) and Mag (magnetometer) are optional.\n"
+"time : numpy.ndarray\n"
+"light : numpy.ndarray\n"
+"starts : numpy.ndarray\n"
+"stops : numpy.ndarray\n";
+
+static const char read_geneactiv__doc__[] = "read_geneactiv(file, bases, periods)\n"
+"Read a Geneactiv File\n\n"
+"Parameters\n"
+"----------\n"
+"file : str\n"
+"   File name to read from\n"
+"bases : numpy.ndarray\n"
+"   Base times for providing windowing. Must be in [0, 23]\n"
+"periods : numpy.ndarray\n"
+"   Number of hours for each window. Must be in [1, 24]\n\n"
+"Returns\n"
+"-------\n"
+"N : int\n"
+"   Number of pages read.\n"
+"fs : float\n"
+"   Sampling frequency\n"
+"accel : numpy.ndarray\n"
+"time : numpy.ndarray\n"
+"light : numpy.ndarray\n"
+"temp : numpy.ndarray\n"
+"starts : numpy.ndarray\n"
+"stops : numpy.ndarray\n";
+
+static struct PyMethodDef methods[] = {
+  {"read_geneactiv", read_geneactiv, 1, read_geneactiv__doc__},
+  {"read_axivity", read_axivity, 1, read_axivity__doc__},
+  {NULL, NULL, 0, NULL}  /* sentinel */
+};
+
+static struct PyModuleDef moduledef = {
+  PyModuleDef_HEAD_INIT,
+  "read",
+  NULL,
+  -1,
+  methods,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+/* Initialization function for the module */
+PyMODINIT_FUNC PyInit_read(void){
+  PyObject *m;
+  m = PyModule_Create(&moduledef);
+  if (m == NULL){
+    return NULL;
+  }
+
+  /* import the array object */
+  import_array();
+
+  /* add constants here */
+
+  return m;
+}
---
+++ src/skimu/read/_extensions/utility.f95
@@ -0,0 +1,110 @@
+! -*- f95 -*-
+
+module custom_time
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    type, bind(C) :: time_t
+        integer(c_long) :: hour
+        integer(c_long) :: min
+        integer(c_long) :: sec
+        integer(c_long) :: msec  ! NOTE that this is a integer!
+    end type time_t
+
+    real(c_double), parameter :: SEC_HOUR = 3600.0
+    real(c_double), parameter :: SEC_MIN = 60.0
+    real(c_double), parameter :: DAY_SEC = 86400.0
+
+contains
+
+    ! =============================================================================================
+    ! get_day_indexing
+    ! 
+    ! Sets the index into a time series any of the window ends fall into the provide data block 
+    ! time frame.
+    ! =============================================================================================
+    subroutine get_day_indexing(fs, dtime, mxd, n, bases, periods, block_n, max_n, block_samples, &
+        starts, i_starts, stops, i_stops) bind(C, name="get_day_indexing")
+        real(c_double), intent(in) :: fs    ! sampling frequency in Hz
+        type(time_t), intent(in) :: dtime   ! storage structure for HH:MM:SS & msec data
+        integer(c_long), intent(in) :: mxd  ! max days/possible windows. Dimension of starts & stops
+        integer(c_long), intent(in) :: n    ! number of windows (bases & periods)
+        integer(c_long), intent(in) :: bases(n)  ! base (start) window hour, 24 hr notation
+        integer(c_long), intent(in) :: periods(n)  ! window duration in hours
+        integer(c_long), intent(in) :: block_n  ! the number of the block currently on
+        integer(c_long), intent(in) :: max_n    ! the maximum number of blocks in the data file
+        integer(c_long), intent(in) :: block_samples  ! the number of data samples per block
+        integer(c_long), intent(inout) :: starts(n, mxd)  ! the start index of windows
+        integer(c_long), intent(inout) :: i_starts(n)  ! keeps track of where in starts we are
+        integer(c_long), intent(inout) :: stops(n, mxd)  ! the stop index of windows
+        integer(c_long), intent(inout) :: i_stops(n)  ! keeps track of where in stops we are
+        ! local
+        integer(c_long) :: i, idx_start, idx_stop
+        real(c_double) :: base_sec, period_sec, dtmp, dtmp2, curr, block_dt
+        logical :: in_win1, in_win2
+
+        curr = dtime%hour * SEC_HOUR + dtime%min * SEC_MIN + dtime%sec + real(dtime%msec, c_double) / 1000.
+
+        block_dt = block_samples / fs
+
+        do i=1, n
+            base_sec = bases(i) * SEC_HOUR
+            dtmp = real(periods(i) + bases(i), c_double)
+            period_sec = mod(dtmp, 24.0) * SEC_HOUR
+
+            ! +1 to account for fortran indexing starting at 1
+            idx_start = i_starts(i) + 1
+            idx_stop = i_stops(i) + 1
+
+            if (block_n == 0) then  ! if the first data block
+                ! Recording start 10:45, windows 0-24, 11-11, 12-6, 8-12, 6-10, 12-11, 11-10
+
+                ! 0-24  yes   10.75 in 0-24  : yes  10.75 in -24-0   : no   || yes
+                ! 11-35 yes   10.75 in 11-35 : no   10.75 in -13-11  : yes  || yes
+                ! 12-18 no    10.75 in 12-18 : no   10.75 in -12--6  : no   || no
+                ! 8-12  yes   10.75 in 8-12  : yes  10.75 in -16--12 : no   || yes
+                ! 6-10  no    10.75 in 6-10  : no   10.75 in -18--14 : no   || no
+                ! 12-35 yes   10.75 in 12-35 : no   10.75 in -12-11  : yes  || yes
+                ! 11-34 no    10.75 in 11-34 : no   10.75 in -13-10  : no   || no
+
+                in_win1 = (curr > base_sec) .and. (curr < (dtmp * SEC_HOUR))
+                in_win2 = (curr > (base_sec - DAY_SEC)) .and. (curr < ((dtmp * SEC_HOUR) - DAY_SEC))
+
+                if (in_win1 .or. in_win2) then
+                    starts(i, 1) = 0
+                    i_starts(i) = i_starts(i) + 1
+                end if
+            end if
+            
+            ! account for differences in fortran/c indexing
+            if ((block_n == max_n) .or. (block_n == (max_n - 1))) then
+                in_win1 = (curr > base_sec) .and. (curr < (dtmp * SEC_HOUR))
+                in_win2 = (curr > (base_sec - DAY_SEC)) .and. (curr < ((dtmp * SEC_HOUR) - DAY_SEC))
+
+                if (in_win1 .or. in_win2) then
+                    stops(i, idx_stop) = max_n * block_samples - 1
+                    ! don't increment idx_stop here so that if it hits here 2x, dont get 2 entries
+                end if
+            end if
+
+            ! check if the (base + period) is during this block
+            dtmp = period_sec - curr
+            dtmp2 = dtmp + DAY_SEC
+
+            if (((dtmp >= 0) .and. (dtmp < block_dt)) .or. (dtmp2 < block_dt)) then
+                stops(i, idx_stop) = block_samples * block_n + int(fs * min(abs(dtmp), abs(dtmp2)), c_long)
+                i_stops(i) = i_stops(i) + 1
+            end if
+
+            ! check if base is during this block
+            dtmp = base_sec - curr
+            dtmp2 = dtmp + DAY_SEC
+
+            if (((dtmp >= 0) .and. (dtmp < block_dt)) .or. (dtmp2 < block_dt)) then
+                starts(i, idx_start) = block_samples * block_n + int(fs * min(abs(dtmp), abs(dtmp2)), c_long)
+                i_starts(i) = i_starts(i) + 1
+            end if
+        end do
+    end subroutine get_day_indexing
+
+end module custom_time
