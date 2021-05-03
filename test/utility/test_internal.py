e9f88c962b7e7e397cc9267afa69c0f3baaac250
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-03 10:08:53 -0400

    Add tests for multiple indices intersection

--- test/utility/test_internal.py
+++ test/utility/test_internal.py
@@ -1,6 +1,51 @@
 import numpy as np
 
-from skimu.utility.internal import rle
+from skimu.utility.internal import rle, get_day_index_intersection
+
+
+class TestGetDayIndexIntersection:
+    day_start = 2000
+    day_stop = 4000
+
+    # treat sleep as exclusionary
+    sleep_starts = {
+        1: np.array([200, 1200, 2200, 4200]),
+        2: np.array([200, 1800, 3800]),
+        3: np.array([200, 1500, 4200])
+    }
+    sleep_stops = {
+        1: np.array([800, 1800, 2800, 5000]),
+        2: np.array([800, 2500, 4400]),
+        3: np.array([200, 1900, 5000])
+    }
+
+    wear_starts = np.array([0, 2300, 3000])
+    wear_stops = np.array([1800, 2900, 3900])
+
+    # solutions
+    starts = {
+        1: np.array([2800, 3000]),
+        2: np.array([2500, 3000]),
+        3: np.array([2300, 3000])
+    }
+    stops = {
+    1: np.array([2900, 4000]),
+    2: np.array([2900, 3800]),
+    3: np.array([2900, 4000])
+    }
+
+    def test(self):
+        for i in range(1, 4):
+            p_starts, p_stops = get_day_index_intersection(
+                (self.sleep_starts[i], self.wear_starts),
+                (self.sleep_stops[i], self.wear_stops),
+                (False, True),
+                self.day_start,
+                self.day_stop
+            )
+
+            assert np.allclose(p_starts, self.starts[i])
+            assert np.allclose(p_stops, self.stops[i])
 
 
 class TestRLE:
