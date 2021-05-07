ffca197c35b6b0fde9c1ccd28bd341b577891fea
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-07 10:24:22 -0400

    Format with python black

--- setup.py
+++ setup.py
@@ -17,13 +17,29 @@ def parse_setuppy_commands():
         # user forgot to give an argument. Let setuptools handle that
         return True
 
-    info_commands = ['--help-commands', '--name', '--version', '-V', 
-                     '--fullname', '--author', '--author-email',
-                     '--maintainer', '--maintainer-email', '--contact', 
-                     '--contact-email', '--url', '--license',
-                     '--description', '--long-description', '--platforms', 
-                     '--classifiers', '--keywords', '--provides',
-                     '--requires', '--obsoletes']
+    info_commands = [
+        "--help-commands",
+        "--name",
+        "--version",
+        "-V",
+        "--fullname",
+        "--author",
+        "--author-email",
+        "--maintainer",
+        "--maintainer-email",
+        "--contact",
+        "--contact-email",
+        "--url",
+        "--license",
+        "--description",
+        "--long-description",
+        "--platforms",
+        "--classifiers",
+        "--keywords",
+        "--provides",
+        "--requires",
+        "--obsoletes",
+    ]
 
     for command in info_commands:
         if command in args:
@@ -32,9 +48,20 @@ def parse_setuppy_commands():
     # Note that 'alias', 'saveopts' and 'setopt' commands also seem to work
     # fine as they are, but are usually used together with one of the commands
     # below and not standalone.  Hence they're not added to good_commands.
-    good_commands = ('develop', 'sdist', 'build', 'build_ext', 'build_py', 
-                     'build_clib', 'build_scripts', 'bdist_wheel',
-                     'bdist_rpm', 'bdist_wininst', 'bdist_msi', 'bdist_mpkg')
+    good_commands = (
+        "develop",
+        "sdist",
+        "build",
+        "build_ext",
+        "build_py",
+        "build_clib",
+        "build_scripts",
+        "bdist_wheel",
+        "bdist_rpm",
+        "bdist_wininst",
+        "bdist_msi",
+        "bdist_mpkg",
+    )
 
     for command in good_commands:
         if command in args:
@@ -42,17 +69,23 @@ def parse_setuppy_commands():
 
     # The following commands are supported, but we need to show more
     # useful messages to the user
-    if 'install' in args:
-        print(textwrap.dedent("""
+    if "install" in args:
+        print(
+            textwrap.dedent(
+                """
             Note: if you need reliable uninstall behavior, then install
             with pip instead of using `setup.py install`:
               - `pip install .`       (from a git repo or downloaded source
                                        release)
-            """))
+            """
+            )
+        )
         return True
 
-    if '--help' in args or '-h' in sys.argv[1]:
-        print(textwrap.dedent("""
+    if "--help" in args or "-h" in sys.argv[1]:
+        print(
+            textwrap.dedent(
+                """
             Help
             -------------------
             To install inertial-sensor-routines from here with reliable 
@@ -62,7 +95,9 @@ def parse_setuppy_commands():
 
             Setuptools commands help
             ------------------------
-            """))
+            """
+            )
+        )
         return False
 
     # The following commands aren't supported.  They can only be executed when
@@ -90,42 +125,55 @@ def parse_setuppy_commands():
         bdist="`setup.py bdist` is not supported",
     )
 
-    bad_commands['nosetests'] = bad_commands['test']
-    for command in ('upload_docs', 'easy_install', 'bdist', 'bdist_dumb',
-                    'register', 'check', 'install_data', 'install_headers',
-                    'install_lib', 'install_scripts',):
+    bad_commands["nosetests"] = bad_commands["test"]
+    for command in (
+        "upload_docs",
+        "easy_install",
+        "bdist",
+        "bdist_dumb",
+        "register",
+        "check",
+        "install_data",
+        "install_headers",
+        "install_lib",
+        "install_scripts",
+    ):
         bad_commands[command] = "`setup.py %s` is not supported" % command
 
     for command in bad_commands.keys():
         if command in args:
-            print(textwrap.dedent(bad_commands[command]) +
-                  "\nAdd `--force` to your command to use it anyway if you "
-                  "must (unsupported).\n")
+            print(
+                textwrap.dedent(bad_commands[command])
+                + "\nAdd `--force` to your command to use it anyway if you "
+                "must (unsupported).\n"
+            )
             sys.exit(1)
 
     # Commands that do more than print info, but also don't need Cython and
     # template parsing.
-    other_commands = ['egg_info', 'install_egg_info', 'rotate']
+    other_commands = ["egg_info", "install_egg_info", "rotate"]
     for command in other_commands:
         if command in args:
             return False
 
     # If we got here, we didn't detect what setup.py command was given
-    warnings.warn("Unrecognized setuptools command ('{}'), proceeding with "
-                  "generating sources and expanding "
-                  "templates".format(' '.join(sys.argv[1:])))
+    warnings.warn(
+        "Unrecognized setuptools command ('{}'), proceeding with "
+        "generating sources and expanding "
+        "templates".format(" ".join(sys.argv[1:]))
+    )
     return True
 
 
 MAINTAINERS = [
-    'Pfizer DMTI Analytics',
-    'Lukas Adamowicz',
-    'Yiorgos Christakis'
+    "Pfizer DMTI Analytics",
+    "Lukas Adamowicz",
+    "Yiorgos Christakis",
 ]
 
 MAINTAINER_EMAILS = [
-    'lukas.adamowicz@pfizer.com',
-    'Yiorgos.Christakis@pfizer.com'
+    "lukas.adamowicz@pfizer.com",
+    "Yiorgos.Christakis@pfizer.com",
 ]
 
 
@@ -150,31 +198,27 @@ with open("requirements.txt", "r") as f:
 REQUIREMENTS = [i.strip() for i in _req]
 
 if sys.version_info < (3, 7):
-    REQUIREMENTS.append('importlib_resources')
-if os.environ.get('CYTHONIZE', 'False') == 'True':
-    REQUIREMENTS.append('cython>=0.29')
+    REQUIREMENTS.append("importlib_resources")
 
 
-def configuration(parent_package='', top_path=None):
+def configuration(parent_package="", top_path=None):
     from numpy.distutils.misc_util import Configuration
-    from pathlib import Path
 
     config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=False)
+    config.set_options(
+        ignore_setup_xxx_py=True,
+        assume_default_configuration=True,
+        delegate_options_to_subpackages=True,
+        quiet=False
+    )
 
     # EXTENSIONS
     # ========================
     # add extension paths all at once
-    config.add_include_dirs(
-        sysconfig.get_path("data") + os.sep + "include"
-    )
+    config.add_include_dirs(sysconfig.get_path("data") + os.sep + "include")
     # UTILITY
     config.add_library(
-        "fmoving_moments",
-        sources="src/skimu/utility/_extensions/moving_moments.f95"
+        "fmoving_moments", sources="src/skimu/utility/_extensions/moving_moments.f95"
     )
     config.add_extension(
         "skimu/utility/_extensions/moving_moments",
@@ -192,25 +236,22 @@ def configuration(parent_package='', top_path=None):
         sources=[
             # utility HAS to be first so that read_axivity can use the module it compiles
             "src/skimu/read/_extensions/utility.f95",
-            'src/skimu/read/_extensions/read_axivity.f95',
-            'src/skimu/read/_extensions/read_geneactiv.c'
+            "src/skimu/read/_extensions/read_axivity.f95",
+            "src/skimu/read/_extensions/read_geneactiv.c",
         ]
     )
     config.add_extension(
-        'skimu/read/_extensions/read',
-        sources='src/skimu/read/_extensions/pyread.c',
-        libraries=['read']
+        "skimu/read/_extensions/read",
+        sources="src/skimu/read/_extensions/pyread.c",
+        libraries=["read"],
     )
 
     # gt3x (actigraph)  needs its own library for some reason
-    config.add_library(
-        'gt3x',
-        sources='src/skimu/read/_extensions/gt3x.c'
-    )
+    config.add_library("gt3x", sources="src/skimu/read/_extensions/gt3x.c")
     config.add_extension(
-        'skimu/read/_extensions/gt3x_convert',
-        sources=['src/skimu/read/_extensions/pygt3x_convert.c'],
-        libraries=['gt3x', 'zip']
+        "skimu/read/_extensions/gt3x_convert",
+        sources=["src/skimu/read/_extensions/pygt3x_convert.c"],
+        libraries=["gt3x", "zip"]
     )
 
     # Fortran/C feature extensions
@@ -221,10 +262,17 @@ def configuration(parent_package='', top_path=None):
             "src/skimu/features/lib/extensions/real_fft.f95",
             "src/skimu/features/lib/extensions/f_rfft.f95",
             "src/skimu/features/lib/extensions/sort.f95",
-            "src/skimu/features/lib/extensions/utility.f95"
+            "src/skimu/features/lib/extensions/utility.f95",
         ]
     )
-    for ext in ["entropy", "frequency", "misc_features", "smoothness", "statistics", "_utility"]:
+    for ext in [
+        "entropy",
+        "frequency",
+        "misc_features",
+        "smoothness",
+        "statistics",
+        "_utility"
+    ]:
         config.add_extension(
             f"skimu/features/lib/extensions/{ext}",
             sources=[f"src/skimu/features/lib/extensions/{ext}.c"],
@@ -235,13 +283,19 @@ def configuration(parent_package='', top_path=None):
     # DATA FILES
     # ========================
     config.add_data_files(
-        ('skimu/gait/model', 'src/skimu/gait/model/final_features.json'),
-        ('skimu/gait/model', 'src/skimu/gait/model/lgbm_gait_classifier_no-stairs_50hz.lgbm'),
-        ('skimu/gait/model', 'src/skimu/gait/model/lgbm_gait_classifier_no-stairs_20hz.lgbm')
+        ("skimu/gait/model", "src/skimu/gait/model/final_features.json"),
+        (
+            "skimu/gait/model",
+            "src/skimu/gait/model/lgbm_gait_classifier_no-stairs_50hz.lgbm",
+        ),
+        (
+            "skimu/gait/model",
+            "src/skimu/gait/model/lgbm_gait_classifier_no-stairs_20hz.lgbm",
+        )
     )
     # ========================
 
-    config.get_version('src/skimu/version.py')
+    config.get_version("src/skimu/version.py")
 
     return config
 
@@ -251,11 +305,11 @@ def setup_package():
 
     with open("README.md", "r") as fh:
         long_description = fh.read()
-    with open('src/skimu/version.py') as fid:
+    with open("src/skimu/version.py") as fid:
         vers = fid.readlines()[-1].split()[-1].strip("\"'")
 
     setup_kwargs = dict(
-        name='scikit-imu',
+        name="scikit-imu",
         maintainer=MAINTAINERS,
         maintainer_email=MAINTAINER_EMAILS,
         description="Python general purpose IMU data processing package.",
@@ -265,8 +319,8 @@ def setup_package():
         # project_urls={
         #     "Documentation": "https://skimu.readthedocs.io./en/latest/"
         # },
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
+        packages=find_packages("src"),
+        package_dir={"": "src"},
         license="GNU GPL v3",
         python_requires=">=3.6",
         setup_requires=REQUIREMENTS,
@@ -276,22 +330,22 @@ def setup_package():
 
     if "--force" in sys.argv:
         run_build = True
-        sys.argv.remove('--force')
+        sys.argv.remove("--force")
     else:
         # raise errors for unsupported commands, improve help output, etc
         run_build = parse_setuppy_commands()
 
     from setuptools import setup
+
     if run_build:
         from numpy.distutils.core import setup
 
-        setup_kwargs['configuration'] = configuration
+        setup_kwargs["configuration"] = configuration
     else:
-        setup_kwargs['version'] = vers
+        setup_kwargs["version"] = vers
 
     setup(**setup_kwargs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
-
