0e357b77eace4c0bc7cc1824411ef697ebdd04fd
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-10 11:04:23 -0400

    Add tests for various inputs for v/ap axis

--- test/utility/test_orientation.py
+++ test/utility/test_orientation.py
@@ -1,5 +1,5 @@
 import pytest
-from numpy import abs, mean
+from numpy import abs, mean, allclose
 
 from skimu.utility.orientation import correct_accelerometer_orientation
 
@@ -12,6 +12,14 @@ class TestCorrectAccelerometerOrientation:
 
         # verify that this has reduced the mis-orientation significantly
         assert mean(abs(x_corr - x_orig)) < 0.1 * mean(abs(x_rot - x_orig))
+
+        # test without providing vertical axis
+        x_corr2 = correct_accelerometer_orientation(x_rot, None, None)
+        assert allclose(x_corr2, x_corr)
+
+        # test with providing ap axis
+        x_corr3 = correct_accelerometer_orientation(x_rot, 2, 0)
+        assert allclose(x_corr3, x_corr)
 
     def test_vaxis_range_error(self):
         with pytest.raises(ValueError):
