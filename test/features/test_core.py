a2634648153367ff31f1daa6ea8df09698810f60
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-10 12:31:53 -0400

    Add tests for normalize_axes

--- test/features/test_core.py
+++ test/features/test_core.py
@@ -58,3 +58,27 @@ class TestNormalizeIndices:
     def test_function_input_error(self):
         with pytest.raises(IndexError):
             normalize_indices(3, get_n_feats)
+
+
+class TestNormalizeAxes:
+    def test_1d(self):
+        assert normalize_axes(1, 0, 500) == (0, None)
+
+    @pytest.mark.parametrize(
+        ("ndim", "ax", "idx_ax", "truth"),
+        (
+                (2, 0, -1, (0, 0)),
+                (2, 0, None, (0, None)),
+                (3, 0, 1, (0, 0)),
+                (3, 1, None, (1, None)),
+                (3, -1, 1, (2, 1)),
+                (4, 1, 3, (1, 2)),
+                (4, 2, 3, (2, 2))
+        )
+    )
+    def test_nd(self, ndim, ax, idx_ax, truth):
+        assert normalize_axes(ndim, ax, idx_ax) == truth
+
+    def test_same_axis(self):
+        with pytest.raises(ValueError):
+            normalize_axes(3, 1, 1)
