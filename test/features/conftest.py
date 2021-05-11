99000f8ff3974f28f752ca7f98a61a91544ebfcc
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-11 10:21:23 -0400

    Added cubic dummy data generator

--- test/features/conftest.py
+++ test/features/conftest.py
@@ -1,7 +1,7 @@
 from tempfile import NamedTemporaryFile
 
 import pytest
-from numpy import zeros
+from numpy import zeros, arange, ndarray
 
 
 @pytest.fixture(scope="class")
@@ -24,3 +24,21 @@ def get_linear_accel(np_rng):
         return x
 
     return get_la
+
+
+@pytest.fixture(scope="module")
+def get_cubic_accel(np_rng):
+    def get_ca(a, b, c, d, scale):
+        x = arange(0, 5, 0.01)
+
+        if isinstance(a, ndarray):
+            a = a.reshape((-1, 1))
+            b = b.reshape((-1, 1))
+            c = c.reshape((-1, 1))
+            d = d.reshape((-1, 1))
+            scale = scale.reshape((-1, 1))
+
+        y = a * x**3 + b * x**2 + c * x + d + scale * np_rng.standard_normal(500)
+        return y
+
+    return get_ca
