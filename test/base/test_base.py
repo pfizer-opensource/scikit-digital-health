c855e58aafa59294c60d532219cef4eebd4e6672
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-17 11:20:38 -0400

    Add tests for _BaseProcess.predict

--- test/base/test_base.py
+++ test/base/test_base.py
@@ -3,6 +3,12 @@ from skimu.base import _BaseProcess
 
 
 class Test_BaseProcess:
+    def test_str_repr(self):
+        bp = _BaseProcess(kw1=1, kw2="2")
+
+        assert str(bp) == "_BaseProcess"
+        assert repr(bp) == "_BaseProcess(kw1=1, kw2='2')"
+
     @staticmethod
     def setup_lgr():
         class Lgr:
@@ -14,9 +20,8 @@ class Test_BaseProcess:
         return Lgr()
 
     def test__check_if_idx_none(self):
-        lgr = self.setup_lgr()
         bp = _BaseProcess()
-        bp.logger = lgr  # overwrite the logger
+        bp.logger = self.setup_lgr()  # overwrite the logger
 
         x = array([[0, 10], [15, 20]])
 
@@ -28,15 +33,24 @@ class Test_BaseProcess:
         s, e = bp._check_if_idx_none(None, "none msg", 0, 10)
         sn, en = bp._check_if_idx_none(None, "none msg", None, 10)
 
-        assert "none msg" in lgr.msgs
+        assert "none msg" in bp.logger.msgs
         assert s == 0
         assert e == 10
 
         assert sn is None
         assert en is None
 
-    def test_str_repr(self):
-        bp = _BaseProcess(kw1=1, kw2="2")
+    def test_predict(self):
+        bp = _BaseProcess()
+        bp.logger = self.setup_lgr()
 
-        assert str(bp) == "_BaseProcess"
-        assert repr(bp) == "_BaseProcess(kw1=1, kw2='2')"
+        bp.predict(
+            expect_days=True,
+            expect_wear=True,
+            accel=array([[1, 2, 3], [4, 5, 6]])
+        )
+
+        assert bp._file_name == ""
+        assert "Entering _BaseProcess processing with call _BaseProcess()" in bp.logger.msgs
+        assert "[_BaseProcess] Day indices [(-1, -1)] not found. No day split used." in bp.logger.msgs
+
