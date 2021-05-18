41c1268e62c6d0f3bcfe28fd9de138017ea206b4
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-18 14:30:02 -0400

    Add tests for warnings and errors

--- test/read/test_read_geneactiv.py
+++ test/read/test_read_geneactiv.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy import allclose
 
 from skimu.read import ReadBin
@@ -19,3 +20,15 @@ class TestReadBin:
             assert allclose(res[k], gnactv_truth[k], atol=5e-5)
 
         assert res["day_ends"] == gnactv_truth["day_ends"]
+
+    def test_none_file_error(self):
+        with pytest.raises(ValueError):
+            ReadBin().predict(None)
+
+    def test_extension_warning(self):
+        with pytest.warns(UserWarning) as record:
+            with pytest.raises(Exception):
+                ReadBin().predict("test.random")
+
+        assert len(record) == 1
+        assert "File extension is not expected '.bin'" in record[0].message.args[0]
