4efc945dd5d6eb4d342585676cbc031fcf844800
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-18 10:57:01 -0400

    Add test with temperature

--- test/preprocessing/conftest.py
+++ test/preprocessing/conftest.py
@@ -32,3 +32,43 @@ def dummy_long_data():
     ap = a / scale - offset
 
     return t, ap, scale, offset
+
+
+@fixture(scope="class")
+def dummy_temp_data():
+    rng = np.random.default_rng(1357)
+
+    # make about 15 hours of data
+    t = np.arange(0, 15 * 3600, 1 / 50)
+
+    # simulate blocks of temperature (ie 1 temp value per block of IMU samples
+    block_temp = sosfiltfilt(
+        butter(1, 0.33, output="sos", btype="low"),
+        (rng.random(t.size // (300 * 50)) - 0.5) * 10,
+    )
+    temp = 29.2 + np.repeat(block_temp, 300 * 50)
+
+    a = (rng.random((t.size, 3)) - 0.5) * 0.07
+    N3 = a.shape[0] // 3
+    a[:N3, 0] = 1 + (rng.random(N3) - 0.5) * 0.15
+    a[N3: 2 * N3, 1] = 0.5 + (rng.random(N3) - 0.4) * 0.15
+    a[2 * N3:, 2] = 0.7 + (rng.random(t.size - 2 * N3) - 0.3) * 0.12
+
+    # rotate 1/6 segements so there are enough points around the sphere
+    N6 = a.shape[0] // 6
+    a[N6: N3 + N6] *= np.array([-1, -1, 1])
+    a[2 * N3 + N6:] *= np.array([-1, 1, -1])
+
+    sos = butter(1, 2 * 1 / 50, btype="low", output="sos")
+    a = sosfiltfilt(sos, a, axis=0)
+
+    a /= np.linalg.norm(a, axis=1, keepdims=True)
+
+    scale = np.array([1.07, 1.05, 0.991])
+    offset = np.array([5.2e-4, -3.8e-5, 1.9e-6])
+    temp_scale = np.array([[-5.6e-05, -4.4e-06, 3.0e-04]])
+
+    # correction: a' = (a + offset) * scale + (temp - mean_temp_cal) * temp_scale
+    acc = (a - (temp - np.mean(temp))[:, None] @ temp_scale) / scale - offset
+
+    return t, acc, temp, scale, offset, temp_scale
--- test/preprocessing/test_calibrate.py
+++ test/preprocessing/test_calibrate.py
@@ -7,7 +7,7 @@ from skimu.preprocessing import CalibrateAccelerometer
 
 
 class TestCalibrateAccelerometer:
-    @pytest.mark.skipif(virtual_memory().available < 90e6, reason="Not enough memory")
+    @pytest.mark.skipif(virtual_memory().available < 90e6, reason="Insufficient memory")
     def test_no_temp(self, dummy_long_data):
         t, acc, true_scale, true_offset = dummy_long_data
 
@@ -22,3 +22,21 @@ class TestCalibrateAccelerometer:
         assert allclose(cal_res["scale"], true_scale, rtol=5e-5)
         # pretty lax here, since offset can be different and still give good values
         assert allclose(cal_res["offset"], true_offset, atol=2e-4)
+
+    @pytest.mark.skipif(virtual_memory().available < 90e6, reason="Insufficient Memory")
+    def test_w_temp(self, dummy_temp_data):
+        t, acc, temp, true_scale, true_offset, true_temp_scale = dummy_temp_data
+
+        error_start = around(mean(abs(norm(acc, axis=1) - 1)), decimals=5)
+
+        cal = CalibrateAccelerometer(min_hours=12)
+        cal_res = cal.predict(t, acc, apply=True, temperature=temp)
+
+        error_end = around(mean(abs(norm(cal_res["accel"], axis=1) - 1)), decimals=5)
+
+        assert error_end < error_start
+        assert allclose(cal_res["scale"], true_scale, rtol=5e-5)
+        # pretty lax here, since offset can be different and still give good values
+        assert allclose(cal_res["offset"], true_offset, atol=2e-4)
+        # pretty lax again, since the values are very small themselves
+        assert allclose(cal_res["temperature scale"], true_temp_scale, atol=2e-4)
