9c82af4aa575132136b1fdb6551f445b1343eb98
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-05-25 09:12:33 -0400

    Add test for gait events

--- test/gait/test_get_gait_events.py
+++ test/gait/test_get_gait_events.py
@@ -1,4 +1,4 @@
-from numpy import isclose, arange, sin, pi
+from numpy import isclose, allclose, arange, sin, pi, zeros
 
 from skimu.gait.get_gait_events import get_cwt_scales, get_gait_events
 
@@ -18,3 +18,17 @@ def test_get_cwt_scales():
     # at 250hz, so scale by 5
     assert isclose(scale1, round(46 / 5))
     assert isclose(scale2, round(79 / 5))
+
+
+def test_get_gait_events():
+    t = arange(0, 5.01, 0.02)
+    x = zeros((t.size, 3))
+    x[:, 0] += 1 + 0.75 * sin(2 * pi * 1.0 * t)
+    x[:, 1] += 0.3 * sin(2 * pi * 1.0 * t)
+    x[:, 2] += 0.1 * sin(2 * pi * 2.0 * t)
+
+    ic, fc, fva, va = get_gait_events(x, 50., t, 8, 4, 20., True, True)
+
+    assert va == 0
+    assert allclose(ic, [13, 63, 113, 163, 213])  # peaks in the sine wave
+    assert allclose(fc, [24, 76, 126, 176, 228])  # peaks in the sine derivative
