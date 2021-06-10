3639f33e5d9680a5dd452b2e47c553069783f044
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-06-10 14:13:27 -0400

    Add a sample file of APDM data

--- test/read/test_apdm.py
+++ test/read/test_apdm.py
@@ -1,13 +1,26 @@
 import pytest
+import h5py
+
+from numpy import allclose
 
 from skdh.read import ReadApdmH5
 
 
 class TestReadBin:
-    def _test(self, apdm_file):
-        res = ReadApdmH5("Lumbar").predict(apdm_file)
+    def test(self, apdm_file):
+        res = ReadApdmH5("Lumbar", gravity_acceleration=9.81).predict(apdm_file)
 
-        # TODO fill this out when an actual file for testing is recieved.
+        lumbar_sens = "XI-010284"
+        with h5py.File(apdm_file) as f:
+            acc = f['Sensors'][lumbar_sens]['Accelerometer'][()] / 9.81
+            time = f['Sensors'][lumbar_sens]["Time"][()] / 1e6  # to seconds
+            gyro = f['Sensors'][lumbar_sens]['Gyroscope'][()]
+            temp = f['Sensors'][lumbar_sens]['Temperature'][()]
+
+        assert allclose(res['accel'], acc)
+        assert allclose(res['time'] - time[0], time - time[0])
+        assert allclose(res['gyro'], gyro)
+        assert allclose(res['temperature'], temp)
 
     def test_none_file_error(self):
         with pytest.raises(ValueError):
