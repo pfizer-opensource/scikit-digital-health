18e24a6903e02af75129cb5c8298327e16aeb45b
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-08-27 12:57:10 -0400

    Add documentation on custom endpoints

--- src/skdh/activity/__init__.py
+++ src/skdh/activity/__init__.py
@@ -86,6 +86,108 @@ acceleration metric.
 
 .. _Using Custom Cutpoints:
 
+Adding Custom Endpoints
+-----------------------
+Custom endpoints are simple to add - each custom endpoint should be a subclass
+of the :class:`.ActivityEndpoint`. Endpoint classes can generate multiple endpoints
+at once, but the names for all of the endpoints need to be specified in the custom
+class, as this is how the results dictionary is populated.
+
+.. code-block:: python
+
+    from skdh.activity import ActivityEndpoint
+
+    class CustomEndpointSingle(ActivityEndpoint):
+        def __init__(self, arg1, arg2=None, state='wake'):
+            super().__init__("custom endpoint name", state)
+
+            self.arg1 = arg1
+            self.arg2 = arg2
+
+        def predict(self, results, i, accel_metric, epoch_s, epochs_per_min, **kwargs):
+            super().predict()
+
+            # desired processing
+
+            # save the results
+            results[self.name][i] = custom_endpoint_res
+
+    class CustomEndpointMultiple(ActivityEndpoint):
+        def __init__(self, arg1, state='wake'):
+            super().__init__(
+                [
+                    "custom ept 1",
+                    "custom ept 2",
+                ],
+                state
+            )
+
+The only required parameter for the custom endpoint class `__init__` is `state`,
+which should be set when initialized to the state in which the endpoint should
+be calculated. The `.predict` method is run for every block of wear data during
+`state`, meaning it could potentially get run multiple times during the same day.
+For some custom endpoints, this may not be a problem, however if one value
+needs to be calculated on all the data for the day, the state of the class
+is kept/left alone between runs. This leads to the below example, which saves
+several values between runs until the full day is done (at which point `.reset_cache()`
+is called:
+
+.. code-block:: python
+
+    class IntensityGradient(ActivityEndpoint):
+        def __init__(self, state="wake"):
+            super(IntensityGradient, self).__init__(
+                ["intensity gradient", "ig intercept", "ig r-squared"], state
+            )
+            # default from rowlands
+            self.ig_levels = (
+                array([i for i in range(0, 4001, 25)] + [8000], dtype="float") / 1000
+            )
+            self.ig_vals = (self.ig_levels[1:] + self.ig_levels[:-1]) / 2
+
+            # values that need to be cached and stored between runs
+            self.hist = zeros(self.ig_vals.size)
+            self.ig = None
+            self.ig_int = None
+            self.ig_r = None
+            self.i = None
+
+        def predict(self, results, i, accel_metric, epoch_s, epochs_per_min, **kwargs):
+            super(IntensityGradient, self).predict()
+            # get the counts in number of minutes in each intensity bin
+            self.hist += (
+                histogram(accel_metric, bins=self.ig_levels, density=False)[0]
+                / epochs_per_min
+            )
+            # get pointers to the intensity gradient results
+            self.ig = results[self.name[0]]
+            self.ig_int = results[self.name[1]]
+            self.ig_r = results[self.name[2]]
+            self.i = i
+
+        def reset_cached(self):
+            super(IntensityGradient, self).reset_cached()
+            # make sure we have results locations to set
+            if all([i is not None for i in [self.ig, self.ig_int, self.ig_r, self.i]]):
+                # compute the results
+                # convert back to mg to match existing work
+                lx = log(self.ig_vals[self.hist > 0] * 1000)
+                ly = log(self.hist[self.hist > 0])
+                if ly.size <= 1:
+                    slope = intercept = rval = nan
+                else:
+                    slope, intercept, rval, *_ = linregress(lx, ly)
+                # set the results values
+                self.ig[self.i] = slope
+                self.ig_int[self.i] = intercept
+                self.ig_r[self.i] = rval ** 2
+            # reset the histogram counts to 0, and results to None
+            self.hist = zeros(self.ig_vals.size)
+            self.ig = None
+            self.ig_int = None
+            self.ig_r = None
+            self.i = None
+
 Using Custom Cutpoints/Metrics
 ------------------------------
 If you would like to use your own custom cutpoints/metric, they can be supplied in a
