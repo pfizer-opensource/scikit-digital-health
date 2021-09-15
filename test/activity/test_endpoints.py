fad6839f0a250ff362cf9bbe39066df3f240c751
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-09-15 10:43:01 -0400

    Move all boutmetric value tests into 1 test

--- test/activity/test_endpoints.py
+++ test/activity/test_endpoints.py
@@ -3,31 +3,21 @@ from numpy import isclose
 from skdh.activity.endpoints import get_activity_bouts, ActivityEndpoint, IntensityGradient, MaxAcceleration, TotalIntensityTime, BoutIntensityTime, FragmentationEndpoints
 
 
-class TestGetActivityBouts:
-    def test_boutmetric_1(self, act_acc):
-        res_a = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 1)
-        res_b = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, False, 1)
+def test_get_activity_bouts(act_acc):
+    res_1_nclosed = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 1)
+    res_1_closed = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, False, 1)
+    res_2 = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 2)
+    res_3 = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 3)
+    res_4 = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 4)
 
-        true_a = 7. + 8. + 6.
-        true_b = 7. + 7. + 6.
+    true_1_nclosed = 21.
+    true_1_closed = 20.
+    true_2 = 24.
+    true_3 = 22.
+    true_4 = 22.
 
-        assert isclose(res_a, true_a)
-        assert isclose(res_b, true_b)
-
-    def test_boutmetric_2(self, act_acc):
-        res_a = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 2)
-        true_a = 24.
-
-        assert isclose(res_a, true_a)
-
-    def test_boutmetric_3(self, act_acc):
-        res_a = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 3)
-        true_a = 22.
-
-        assert isclose(res_a, true_a)
-
-    def test_boutmetric_4(self, act_acc):
-        res_a = get_activity_bouts(act_acc, 0.5, 1.5, 60, 6, 0.8, True, 4)
-        true_a = 22.
-
-        assert isclose(res_a, true_a)
+    assert isclose(res_1_nclosed, true_1_nclosed)
+    assert isclose(res_1_closed, true_1_closed)
+    assert isclose(res_2, true_2)
+    assert isclose(res_3, true_3)
+    assert isclose(res_4, true_4)
