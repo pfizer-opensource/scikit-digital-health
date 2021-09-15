4439cd3a86aaeca80c5bf948c003f1fb4ce0f5f3
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-09-15 14:36:32 -0400

    Add tests for the activity endpoints

--- test/activity/conftest.py
+++ test/activity/conftest.py
@@ -7,3 +7,60 @@ def act_acc():
     x = array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1])
 
     return x
+
+
+@fixture(scope='function')  # function so it gets reset for each test
+def act_results():
+    k = [
+        'wake intensity gradient',
+        'wake ig intercept',
+        'wake ig r-squared',
+        'wake max acc 2min [g]',
+        'wake MVPA 5s epoch [min]',
+        'wake MVPA 6min bout [min]',
+        'wake MVPA avg duration',
+        'wake MVPA transition probability',
+        'wake MVPA gini index',
+        'wake MVPA avg hazard',
+        'wake MVPA power law distribution',
+    ]
+
+    return {i: [0.0] for i in k}
+
+
+@fixture(scope="module")
+def dummy_frag_predictions():
+    return array(
+        [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            0,
+            0,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
+    )
