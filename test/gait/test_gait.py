83ff31dc3ded9bed4a0fcf9cf0a3d1b9df5f94b7
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-10-22 11:30:44 -0400

    Update gait results for fixed index intersection code

--- test/gait/test_gait.py
+++ test/gait/test_gait.py
@@ -24,9 +24,7 @@ class TestGait:
             day_window=(0, 24),
         )
 
-        # silence some warnings
-        with pytest.warns(RuntimeWarning):
-            res = g.predict(time=t, accel=acc, fs=50.0, height=1.88)
+        res = g.predict(time=t, accel=acc, fs=50.0, height=1.88)
 
         for key in gait_res_50.files:
             assert allclose(res[key], gait_res_50[key], equal_nan=True)
