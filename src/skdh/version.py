e512df97ce4f68e6f47c756bead501003e6211cf
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2021-11-16 14:44:22 -0500

    Bump version

--- src/skdh/version.py
+++ src/skdh/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 
 # ==============================================================================
 # MINIMUM VERSION FOR GUARANTEED BACKWARDS COMPATIBILITY
