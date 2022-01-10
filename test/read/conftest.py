746adf9b5195a4a2c5dc9ec5c3497a31388419ee
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2022-01-10 10:15:29 -0500

    Don't check size for testing

--- test/read/conftest.py
+++ test/read/conftest.py
@@ -22,7 +22,7 @@ def dummy_reader_class():
             else:
                 raise ValueError("`ext_error` must be one of 'raise', 'warn', 'skip'.")
 
-        @check_input_file(".abc")
+        @check_input_file(".abc", check_size=False)
         def predict(self, file=None, **kwargs):
             super().predict(expect_wear=False, expect_days=False, file=file, **kwargs)
 
