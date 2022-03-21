1b83a8db5fc020503ea0fe73d077e2de735952fc
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2022-03-21 13:40:36 -0400

    Add a file with the commands that get run to build versions of SKDH

---
+++ build_scripts/dist.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Builds both a wheel and a distribution
+python -m build
+# If building only a wheel
+python -m build --wheel
+# if building only a source dist
+python -m build --sdist
+
+#########
+# delocate for mac wheels
+# list dependencies
+delocate-listdeps --depending dist/*.whl
+# fix dependencies
+delocate-wheel -w fixed_wheels -v dist/*.whl
