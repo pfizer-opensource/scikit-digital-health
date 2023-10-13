3daf15ec979fce47778cb2703edf22f7dc23902c
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2023-10-13 13:11:08 -0400

    Black formatting

--- src/skdh/gaitv3/substeps/s1_preprocessing.py
+++ src/skdh/gaitv3/substeps/s1_preprocessing.py
@@ -26,6 +26,7 @@ class PreprocessGaitBout(BaseProcess):
     filter_order : int, optional
         Low-pass filter order. Default is 4.
     """
+
     def __init__(self, correct_orientation=True, filter_cutoff=20.0, filter_order=4):
         super().__init__(
             correct_orientation=correct_orientation,
@@ -55,21 +56,23 @@ class PreprocessGaitBout(BaseProcess):
         ap_axis_sign : {-1, 1}
             Sign of the AP axis.
         """
-        sos = butter(4, [2 * 0.25 / fs, 2 * 7.0 / fs], output='sos', btype='band')
+        sos = butter(4, [2 * 0.25 / fs, 2 * 7.0 / fs], output="sos", btype="band")
         ap_acc_f = sosfiltfilt(sos, accel[:, ap_axis])
 
         mx, mx_meta = find_peaks(ap_acc_f, prominence=0.05)
-        med_prom = median(mx_meta['prominences'])
-        mask = mx_meta['prominences'] > (0.75 * med_prom)
+        med_prom = median(mx_meta["prominences"])
+        mask = mx_meta["prominences"] > (0.75 * med_prom)
 
-        left_med = median(mx[mask] - mx_meta['left_bases'][mask])
-        right_med = median(mx_meta['right_bases'][mask] - mx[mask])
+        left_med = median(mx[mask] - mx_meta["left_bases"][mask])
+        right_med = median(mx_meta["right_bases"][mask] - mx[mask])
 
         sign = -1 if (left_med < right_med) else 1
 
         return sign
 
-    def predict(self, time=None, accel=None, *, fs=None, v_axis=None, ap_axis=None, **kwargs):
+    def predict(
+        self, time=None, accel=None, *, fs=None, v_axis=None, ap_axis=None, **kwargs
+    ):
         """
         predict(time, accel, *, fs=None, v_axis=None, ap_axis=None)
 
@@ -106,14 +109,11 @@ class PreprocessGaitBout(BaseProcess):
         v_axis_sign = sign(acc_mean[v_axis])
 
         if ap_axis is None:
-            sos = butter(4, 2 * 3.0 / fs, output='sos')
+            sos = butter(4, 2 * 3.0 / fs, output="sos")
             acc_f = sosfiltfilt(sos, accel, axis=0)
 
             ac = gait_endpoints._autocovariancefn(
-                acc_f,
-                min(accel.shape[0] - 1, int(10 * fs)),
-                biased=True,
-                axis=0
+                acc_f, min(accel.shape[0] - 1, int(10 * fs)), biased=True, axis=0
             )
 
             ap_axis = argsort(corrcoef(ac.T)[v_axis])[-2]
@@ -122,24 +122,25 @@ class PreprocessGaitBout(BaseProcess):
         ap_axis_sign = self.get_ap_axis_sign(fs, accel, ap_axis)
 
         if self.corr_orient:
-            accel = correct_accelerometer_orientation(accel, v_axis=v_axis, ap_axis=ap_axis)
+            accel = correct_accelerometer_orientation(
+                accel, v_axis=v_axis, ap_axis=ap_axis
+            )
 
         # filter
-        sos = butter(self.filter_order, 2 * self.filter_cutoff / fs, output='sos', btype='low')
+        sos = butter(
+            self.filter_order, 2 * self.filter_cutoff / fs, output="sos", btype="low"
+        )
         ap_acc_f = sosfiltfilt(sos, accel[:, ap_axis])
 
         # detrend
         ap_acc_f = detrend(ap_acc_f)
 
         # estimate step frequency
-        sos = butter(4, 2 * 10.0 / fs, output='sos')
+        sos = butter(4, 2 * 10.0 / fs, output="sos")
         sf_acc_f = sosfiltfilt(sos, accel, axis=0)
 
         ac = gait_endpoints._autocovariancefn(
-            sf_acc_f,
-            min(sf_acc_f.shape[0] - 1, int(4 * fs)),
-            biased=True,
-            axis=0
+            sf_acc_f, min(sf_acc_f.shape[0] - 1, int(4 * fs)), biased=True, axis=0
         )
 
         factor = 1.0
@@ -157,15 +158,14 @@ class PreprocessGaitBout(BaseProcess):
             {
                 self._time: time,
                 self._acc: accel,
-                'fs': fs,
-                'v_axis': v_axis,
-                'v_axis_sign': v_axis_sign,
-                'ap_axis': ap_axis,
-                'ap_axis_sign': ap_axis_sign,
-                'mean_step_freq': mean_step_freq,
-                'ap_accel': ap_acc_f,
+                "fs": fs,
+                "v_axis": v_axis,
+                "v_axis_sign": v_axis_sign,
+                "ap_axis": ap_axis,
+                "ap_axis_sign": ap_axis_sign,
+                "mean_step_freq": mean_step_freq,
+                "ap_accel": ap_acc_f,
             }
         )
 
         return (kwargs, None) if self._in_pipeline else kwargs
-
