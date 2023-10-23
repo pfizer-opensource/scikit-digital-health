93a4c9a75914accc26069b5e3e0e0a89202cc99f
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2023-10-23 08:23:03 -0400

    Fix test class names and update gait prediction handling test

--- test/gaitv3/test_core.py
+++ test/gaitv3/test_core.py
@@ -64,7 +64,7 @@ class TestGait:
             assert allclose(res[key], gait_res_gyro[key], equal_nan=True), key
 
     def test_add_metrics(self):
-        g = Gait()
+        g = GaitLumbar()
         g._params = []  # reset for easy testing
 
         g.add_endpoints([gait_endpoints.StrideTime, gait_endpoints.StepTime])
@@ -77,39 +77,48 @@ class TestGait:
         ]
 
     def test_add_metrics_error(self):
-        g = Gait()
+        g = GaitLumbar()
 
         with pytest.raises(ValueError):
-            g.add_endpoints([list, Gait])
+            g.add_endpoints([list, GaitLumbar])
 
         with pytest.raises(ValueError):
-            g.add_endpoints(Gait)
+            g.add_endpoints(GaitLumbar)
 
     def test_leg_length_factor(self):
-        g = Gait(prov_leg_length=True, height_factor=0.53)
+        g = GaitLumbar(provide_leg_length=True, height_factor=0.53)
 
         assert g.height_factor == 1.0
 
     def test__handle_input_gait_predictions(self):
-        starts, stops = Gait._handle_input_gait_predictions(None, 10)
-        assert starts is None and stops is None
-
-        starts, stops = Gait._handle_input_gait_predictions(True, 10)
+        # ====================================
+        # gait_pred values
+        starts, stops = GaitLumbar._handle_input_gait_predictions(None, True, 10)
         assert allclose(starts, [0])
         assert allclose(stops, [9])
 
-        starts, stops = Gait._handle_input_gait_predictions(
-            array([0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0]), 12
+        starts, stops = GaitLumbar._handle_input_gait_predictions(
+            None, array([0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0]), 12
         )
         assert allclose(starts, [3, 10])
         assert allclose(stops, [9, 11])
 
         with pytest.raises(ValueError):
-            Gait._handle_input_gait_predictions(array([1, 1, 1, 0, 0, 1]), 15)
+            GaitLumbar._handle_input_gait_predictions(None, array([1, 1, 1, 0, 0, 1]), 15)
+
+        # ====================================
+        # gait_bout values
+        starts, stops = GaitLumbar._handle_input_gait_predictions(array([[0, 4], [6, 8]]), None, 10)
+        assert allclose(starts, [0, 6])
+        assert allclose(stops, [4, 8])
+
+        starts, stops = GaitLumbar._handle_input_gait_predictions(array([[0, 4], [6, 8]]), True, 10)
+        assert allclose(starts, [0, 6])
+        assert allclose(stops, [4, 8])
 
     def test_no_height_low_frequency(self):
         t = array([1, 2, 3])
         a = array([[1, 2], [3, 4], [5, 6]])
         with pytest.raises(LowFrequencyError):
             with pytest.warns(UserWarning):
-                Gait().predict(time=t, accel=a, fs=10.0, height=None)
+                GaitLumbar().predict(time=t, accel=a, fs=10.0, height=None)
