e294f86b9a09e3c197d8810f9dba903b00778ef6
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2024-04-09 10:13:18 -0400

    add check for too small computation axis when computing bouts

--- src/skdh/activity/endpoints.py
+++ src/skdh/activity/endpoints.py
@@ -170,7 +170,10 @@ def get_activity_bouts(
         N = int(60 / wlen) + 1
         i1 = int(floor((N + 1) / 2)) - 1
         i2 = int(ceil(x.size - N / 2))
-        lookforbreaks[i1:i2] = moving_mean(x, N, 1)
+        try:
+            lookforbreaks[i1:i2] = moving_mean(x, N, 1)
+        except ValueError:
+            pass  # just leave as all zeros since we dont have data
         # insert negative numbers to prevent these minutes from being counted in bouts
         xt[lookforbreaks == 0] = -(60 / wlen) * nboutdur
         # in this way there will not be bout breaks lasting longer than 1 minute
